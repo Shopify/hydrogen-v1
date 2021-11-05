e9237990deb256d706dee3e4297311c946310f81
Ben Wolfram <ben.wolfram@shopify.com>
2021-11-05 10:31:28 -0500

    wip: update product page styles

--- packages/dev/src/components/ProductOptions.client.jsx
+++ packages/dev/src/components/ProductOptions.client.jsx
@@ -7,8 +7,8 @@ export default function ProductOptions() {
     <>
       {options.map(({name, values}) => {
         return (
-          <fieldset key={name} className="mb-8">
-            <legend className="mb-2 d:mb-4 md:text-xl font-medium text-gray-900">
+          <fieldset key={name} className="mt-8">
+            <legend className="mb-4 text-xl font-medium text-gray-900">
               {name}
             </legend>
             <div className="flex items-center flex-wrap gap-4">
@@ -28,7 +28,7 @@ export default function ProductOptions() {
                       onChange={() => setSelectedOption(name, value)}
                     />
                     <div
-                      className={`p-2 border cursor-pointer text-sm md:text-md ${
+                      className={`p-2 border cursor-pointer rounded text-sm md:text-md ${
                         checked ? 'bg-gray-900 text-white' : 'text-gray-900'
                       }`}
                     >
