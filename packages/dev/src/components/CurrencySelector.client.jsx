6612c961bfa0f149268e103eb339fd58c09ce382
Ben Wolfram <ben.wolfram@shopify.com>
2021-11-05 12:34:53 -0500

    Update packages/dev/src/components/CurrencySelector.client.jsx
    Co-authored-by: Cathryn Griffiths <cathryn.griffiths@shopify.com>

--- packages/dev/src/components/CurrencySelector.client.jsx
+++ packages/dev/src/components/CurrencySelector.client.jsx
@@ -62,6 +62,7 @@ function CheckIcon() {
       viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
     >
       <path
         d="M7 10L9 12L13 8M19 10C19 14.9706 14.9706 19 10 19C5.02944 19 1 14.9706 1 10C1 5.02944 5.02944 1 10 1C14.9706 1 19 5.02944 19 10Z"
