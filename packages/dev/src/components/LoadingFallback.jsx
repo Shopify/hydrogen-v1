4d57c23362aa615b6f0cce7b586e3c9613c33b23
Travis Amaral <travis.amaral@shopify.com>
2021-11-05 09:31:12 -0700

    wip: update styles

--- packages/dev/src/components/LoadingFallback.jsx
+++ packages/dev/src/components/LoadingFallback.jsx
@@ -4,7 +4,7 @@ import OpenIcon from './OpenIcon';
 export default function LoadingFallback() {
   return (
     <header className="h-20 lg:h-32 max-w-screen text-gray-700">
-      <div className="fixed z-10 h-20 lg:h-32 w-full bg-white border-b-2 border-black px-6 md:px-8 md:py-6 lg:pt-8 lg:pb-0 mx-auto">
+      <div className="fixed z-10 h-20 lg:h-32 w-full bg-white/90 border-b border-black border-opacity-5 px-6 md:px-8 md:py-6 lg:pt-8 lg:pb-0 mx-auto">
         <div className="h-full flex lg:flex-col place-content-between">
           <div className="text-center w-full flex justify-between items-center">
             <div className="hidden lg:block w-16" />
