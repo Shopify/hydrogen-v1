5ae43aabda1541b4903946b0d8066f50aec65df5
Scott Dixon <59898611+scottdixon@users.noreply.github.com>
2021-11-20 01:55:48 +1000

    docs: Model3d poster props (#275)

--- packages/hydrogen/src/components/Model3D/Model3D.client.tsx
+++ packages/hydrogen/src/components/Model3D/Model3D.client.tsx
@@ -28,6 +28,8 @@ export interface Model3DProps {
   };
   /** A string of either `auto`, `lazy`, or `eager` to indicate the conditions for preloading. Refer to [`loading` in the <model-viewer> documentation](https://modelviewer.dev/docs/index.html#entrydocs-loading-attributes-loading). */
   loading?: 'auto' | 'lazy' | 'eager';
+  /** A url to display an image instead of the model, useful for showing the user something before a model is loaded and ready to render. If none is provided, [Model3d.previewImage](https://shopify.dev/api/storefront/reference/products/model3d#previewimage-2021-10) is used. Refer to [`poster` in the <model-viewer> documentation](https://modelviewer.dev/docs/index.html#entrydocs-loading-attributes-poster). */
+  poster?: string;
   /** A string of either `auto`, `interaction`, or `manual` to indicate when the model should be revealed. Refer to [`reveal` in the <model-viewer> documentation](https://modelviewer.dev/docs/index.html#entrydocs-loading-attributes-reveal). */
   reveal?: 'auto' | 'interaction' | 'manual';
   /** A boolean to enable an AR experience. Refer to [`ar` in the <model-viewer> documentation](https://modelviewer.dev/docs/index.html#entrydocs-augmentedreality-attributes-ar). */
