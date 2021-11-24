e74b18b4edacd03c1b8f2e9ca09eb4ce27933022
Fran Dios <frankdiox@gmail.com>
2021-11-25 00:12:37 +0900

    fix: avoid repeating the same identifier for default and named exports (#302)
    Co-authored-by: Josh Larson <josh.larson@shopify.com>

--- packages/hydrogen/src/framework/server-components.ts
+++ packages/hydrogen/src/framework/server-components.ts
@@ -3,6 +3,8 @@ import {promises as fs} from 'fs';
 import {transformWithEsbuild} from 'vite';
 import MagicString from 'magic-string';
 
+const DEFAULT_EXPORT = 'default';
+
 export async function proxyClientComponent({
   id,
   src,
@@ -10,8 +12,6 @@ export async function proxyClientComponent({
   id: string;
   src?: string;
 }) {
-  const defaultComponentName = id.split('/').pop()?.split('.').shift();
-
   // Modify the import ID to avoid infinite wraps
   const importFrom = `${id}?no-proxy`;
 
@@ -23,92 +23,48 @@ export async function proxyClientComponent({
 
   const {code} = await transformWithEsbuild(src, id);
   const [, exportStatements] = parse(code);
-  const hasDefaultExport = exportStatements.includes('default');
 
-  // Split namedImports in components to wrap and everything else (e.g. GQL Fragments)
-  const namedImports = exportStatements.reduce(
-    (acc, i) => {
-      if (i !== 'default') {
-        // Add here any other naming pattern for a non-component export
-        if (/^use[A-Z]|Fragment$|Context$|^[A-Z_]+$/.test(i)) {
-          acc.other.push(i);
-        } else {
-          acc.components.push(i);
-        }
-      }
+  // Classify exports in components to wrap vs. everything else (e.g. GQL Fragments)
+  const otherExports = [] as string[];
+  const componentExports = [] as string[];
+  for (const key of exportStatements) {
+    if (
+      key !== DEFAULT_EXPORT &&
+      /^use[A-Z]|Fragment$|Context$|^[A-Z_]+$/.test(key)
+    ) {
+      otherExports.push(key);
+    } else {
+      componentExports.push(key);
+    }
+  }
 
-      return acc;
-    },
-    {components: [] as string[], other: [] as string[]}
-  );
-
-  if (!hasDefaultExport && namedImports.components.length === 0) {
+  if (componentExports.length === 0) {
     return `export * from '${importFrom}';\n`;
   }
 
   const s = new MagicString(
-    `import {wrapInClientMarker} from '@shopify/hydrogen/marker';`
+    `import {wrapInClientMarker} from '@shopify/hydrogen/marker';\n` +
+      `import * as allImports from '${importFrom}';\n\n`
   );
-
-  s.append('\nimport ');
-
-  if (hasDefaultExport) {
-    s.append(defaultComponentName!);
-    if (namedImports.components.length > 0) {
-      s.append(', ');
-    }
-  }
-
-  if (namedImports.components.length) {
-    s.append('* as namedImports');
-  }
-
-  s.append(` from '${importFrom}';\n\n`);
 
   // Re-export other stuff directly without wrapping
-  if (namedImports.other.length > 0) {
-    s.append(
-      `export {${namedImports.other.join(', ')}} from '${importFrom}';\n`
-    );
+  if (otherExports.length > 0) {
+    s.append(`export {${otherExports.join(', ')}} from '${importFrom}';\n`);
   }
 
-  if (hasDefaultExport) {
-    s.append(
-      generateComponentExport({
-        id,
-        componentName: defaultComponentName!,
-        isDefault: true,
-      })
-    );
-  }
+  // Wrap components in Client Marker
+  componentExports.forEach((key) => {
+    const isDefault = key === DEFAULT_EXPORT;
+    const componentName = isDefault
+      ? (id.split('/').pop()?.split('.').shift() as string)
+      : key;
 
-  namedImports.components.forEach((name) =>
     s.append(
-      generateComponentExport({
-        id,
-        componentName: name,
-        isDefault: false,
-      })
-    )
-  );
+      `export ${
+        isDefault ? DEFAULT_EXPORT : `const ${componentName} =`
+      } wrapInClientMarker({ name: '${componentName}', id: '${id}', component: allImports['${key}'], named: ${!isDefault} });\n`
+    );
+  });
 
   return s.toString();
-}
-
-function generateComponentExport({
-  id,
-  isDefault,
-  componentName,
-}: {
-  id: string;
-  isDefault: boolean;
-  componentName: string;
-}) {
-  const component = isDefault
-    ? componentName
-    : `namedImports['${componentName}']`;
-
-  return `export ${
-    isDefault ? 'default' : `const ${componentName} =`
-  } wrapInClientMarker({ name: '${componentName}', id: '${id}', component: ${component}, named: ${!isDefault} });\n`;
 }
