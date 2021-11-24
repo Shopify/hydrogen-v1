0fbabd0c7eb96c72582022d48259a76584a8f348
Fran Dios <fran.dios@shopify.com>
2021-11-24 18:34:10 +0900

    test: secrets in development environment

--- packages/playground/server-components/src/pages/index.server.jsx
+++ packages/playground/server-components/src/pages/index.server.jsx
@@ -1,12 +1,14 @@
 import {Link} from '@shopify/hydrogen';
 
-export default function Index() {
+export default function Index({secrets}) {
   return (
     <>
       <h1>Home</h1>
       <Link className="btn" to="/about">
         About
       </Link>
+
+      <div className="secrets">{JSON.stringify(secrets)}</div>
     </>
   );
 }
