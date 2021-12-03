f7f6b51a7d3431489129be187da8bf19d57e92eb
Fran Dios <fran.dios@shopify.com>
2021-12-03 15:08:51 +0900

    wip: fix test app

--- packages/dev/src/RSCTest/C2.client.jsx
+++ packages/dev/src/RSCTest/C2.client.jsx
@@ -1,10 +1,10 @@
 const onClick = () => window.alert('42');
 
 export default function C2(props) {
-  const color = props.myProp2 ? 'blue' : 'gray';
+  const color = props.myProp2 ? 'bg-blue-500' : 'bg-gray-500';
   return (
     <button
-      className={`bg-${color}-500  text-white py-2 px-4 rounded`}
+      className={`${color} text-white py-2 px-4 rounded`}
       onClick={onClick}
     >
       c-2
--- packages/dev/src/RSCTest/C3.client.jsx
+++ packages/dev/src/RSCTest/C3.client.jsx
@@ -2,10 +2,10 @@ const onClick = () => window.alert('42');
 
 // NAMED EXPORT
 export function C3(props) {
-  const color = props.myProp3 ? 'blue' : 'gray';
+  const color = props.myProp3 ? 'bg-blue-500' : 'bg-gray-500';
   return (
     <button
-      className={`bg-${color}-500  text-white py-2 px-4 rounded`}
+      className={`${color}  text-white py-2 px-4 rounded`}
       onClick={onClick}
     >
       c-3
