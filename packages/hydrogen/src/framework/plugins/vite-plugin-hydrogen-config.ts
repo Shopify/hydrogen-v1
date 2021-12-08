cc5f7b7c6c652b35bb6b1ebf5f8c5299e8f1c67b
Fran Dios <fran.dios@shopify.com>
2021-12-08 14:08:57 +0900

    feat: accept import.meta.env.PUBLIC_ instead of Oxygen.env.HYDROGEN_PUBLIC_

--- packages/hydrogen/src/framework/plugins/vite-plugin-hydrogen-config.ts
+++ packages/hydrogen/src/framework/plugins/vite-plugin-hydrogen-config.ts
@@ -1,85 +1,69 @@
-import {loadEnv, Plugin} from 'vite';
+import {Plugin} from 'vite';
 
 export default () => {
   return {
     name: 'vite-plugin-hydrogen-config',
-    config: async (config, env) => {
-      const oxygenEnvReplacements = {} as Record<string, string>;
-
-      const oxygenEnv = await loadEnv(
-        'build',
-        config.root || '',
-        'HYDROGEN_PUBLIC_'
-      );
-
-      for (const [key, value] of Object.entries(oxygenEnv)) {
-        oxygenEnvReplacements['Oxygen.env.' + key] = JSON.stringify(value);
-      }
-
-      return {
-        resolve: {
-          alias: {
-            /**
-             * For some reason, when building in a worker, Vite always
-             * pulls the client version instead of the server version of these
-             * dependencies. We intentially force it to load the server versions.
-             */
-            'html-dom-parser': process.env.WORKER
-              ? 'html-dom-parser/lib/server/html-to-dom'
-              : 'html-dom-parser',
-          },
-        },
-
-        build: {
-          sourcemap: true,
-        },
-
-        ssr: {
-          external: ['isomorphic-dompurify'],
+    config: async (config, env) => ({
+      resolve: {
+        alias: {
           /**
-           * Tell Vite to bundle everything when we're building for Workers.
+           * For some reason, when building in a worker, Vite always
+           * pulls the client version instead of the server version of these
+           * dependencies. We intentially force it to load the server versions.
            */
-          noExternal: Boolean(process.env.WORKER),
-          target: process.env.WORKER ? 'webworker' : 'node',
+          'html-dom-parser': process.env.WORKER
+            ? 'html-dom-parser/lib/server/html-to-dom'
+            : 'html-dom-parser',
         },
+      },
 
-        // Reload when updating local Hydrogen lib
-        server: process.env.LOCAL_DEV && {
-          watch: {
-            ignored: ['!**/node_modules/@shopify/hydrogen/**'],
-          },
-        },
+      build: {
+        sourcemap: true,
+      },
 
-        optimizeDeps: {
-          exclude: [
-            '@shopify/hydrogen/client',
-            '@shopify/hydrogen/entry-client',
-          ],
-          include: [
-            /**
-             * Additionally, the following dependencies have trouble loading the
-             * correct version of the dependency (server vs client). This tells Vite to take the
-             * server versions and optimize them for ESM.
-             */
-            'html-dom-parser',
-            'html-react-parser',
-            'react-helmet-async',
-            /**
-             * Vite cannot find the following dependencies since they might be
-             * required in RSC asynchronously. This tells Vite to optimize them
-             * at server start to avoid posterior page reloads and issues (#429 #430).
-             */
-            'react',
-            'react-dom',
-            'react-router-dom',
-          ],
-        },
+      ssr: {
+        external: ['isomorphic-dompurify'],
+        /**
+         * Tell Vite to bundle everything when we're building for Workers.
+         */
+        noExternal: Boolean(process.env.WORKER),
+        target: process.env.WORKER ? 'webworker' : 'node',
+      },
 
-        define: {
-          ...oxygenEnvReplacements,
-          __DEV__: env.mode !== 'production',
+      // Reload when updating local Hydrogen lib
+      server: process.env.LOCAL_DEV && {
+        watch: {
+          ignored: ['!**/node_modules/@shopify/hydrogen/**'],
         },
-      };
-    },
+      },
+
+      optimizeDeps: {
+        exclude: ['@shopify/hydrogen/client', '@shopify/hydrogen/entry-client'],
+        include: [
+          /**
+           * Additionally, the following dependencies have trouble loading the
+           * correct version of the dependency (server vs client). This tells Vite to take the
+           * server versions and optimize them for ESM.
+           */
+          'html-dom-parser',
+          'html-react-parser',
+          'react-helmet-async',
+          /**
+           * Vite cannot find the following dependencies since they might be
+           * required in RSC asynchronously. This tells Vite to optimize them
+           * at server start to avoid posterior page reloads and issues (#429 #430).
+           */
+          'react',
+          'react-dom',
+          'react-router-dom',
+        ],
+      },
+
+      define: {
+        __DEV__: env.mode !== 'production',
+      },
+
+      envPrefix: ['VITE_', 'PUBLIC_'],
+    }),
   } as Plugin;
 };
