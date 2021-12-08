c739ed4b1035214479433786048cd9c298abe04e
Fran Dios <fran.dios@shopify.com>
2021-12-08 16:59:32 +0900

    chore: allow merge commit messages

--- scripts/verify-commit.js
+++ scripts/verify-commit.js
@@ -9,10 +9,11 @@ const msgPath = process.env.GIT_PARAMS;
 const msg = require('fs').readFileSync(msgPath, 'utf-8').trim();
 
 const releaseRE = /^v\d/;
+const mergeRE = /^Merge branch/;
 const commitRE =
   /^(revert: )?(feat|fix|docs|dx|refactor|perf|test|workflow|build|ci|chore|types|wip|release|deps)(\(.+\))?: .{1,50}/;
 
-if (!releaseRE.test(msg) && !commitRE.test(msg)) {
+if (!releaseRE.test(msg) && !commitRE.test(msg) && !mergeRE.test(msg)) {
   console.log();
   console.error(
     `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
