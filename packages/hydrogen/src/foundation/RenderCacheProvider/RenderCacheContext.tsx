2f85958d5bbd0666bc27ea1ac4b0148cbaa6d4a4
Helen Lin <helen.lin@shopify.com>
2022-01-06 12:27:04 -0800

    feat: clear default render cache after timeout

--- packages/hydrogen/src/foundation/RenderCacheProvider/RenderCacheContext.tsx
+++ packages/hydrogen/src/foundation/RenderCacheProvider/RenderCacheContext.tsx
@@ -1,6 +1,20 @@
 import {createContext} from 'react';
 import type {RenderCacheProviderProps} from './types';
 
+let clearDefaultCacheTimeout: any;
+
 export const RenderCacheContext = createContext<RenderCacheProviderProps>({
-  cache: {},
+  cache: new Proxy({} as RenderCacheProviderProps['cache'], {
+    get(target, key: string) {
+      clearTimeout(clearDefaultCacheTimeout);
+      clearDefaultCacheTimeout = setTimeout(() => {
+        target = {};
+      }, 3000);
+      return target[key];
+    },
+    set(target, key: string, value) {
+      target[key] = value;
+      return true;
+    },
+  }),
 });
