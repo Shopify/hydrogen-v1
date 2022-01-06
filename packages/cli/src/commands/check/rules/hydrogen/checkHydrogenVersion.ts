449d6b764e656d83a8c6347830a2afaffcb3a6e5
Matt Seccafien <mseccafien@gmail.com>
2022-01-07 00:27:05 +0100

    fix: hydrogen version check

--- packages/cli/src/commands/check/rules/hydrogen/checkHydrogenVersion.ts
+++ packages/cli/src/commands/check/rules/hydrogen/checkHydrogenVersion.ts
@@ -1,27 +1,35 @@
 import semver from 'semver';
 import {Env, CheckResult} from '../../../../types';
-import addHydrogen from '../../../addons/hydrogen';
+import addHydrogen from '../../../add/hydrogen';
 
-const HYDROGEN_MIN_VERSION = '0.0.6';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const HYDROGEN_MIN_VERSION = require('../../../../../package.json').version;
 
 export async function checkHydrogenVersion({
   workspace,
 }: Env): Promise<CheckResult[]> {
   const h2Version = await workspace.hasDependency('@shopify/hydrogen');
-  const normalizedVersion = h2Version && semver.coerce(h2Version)?.version;
+  const normalizedVersion = h2Version
+    ? semver.coerce(h2Version)?.version
+    : `@shopify/hydrogen not installed`;
+  const latestHydrogen =
+    typeof h2Version === 'string' &&
+    typeof normalizedVersion === 'string' &&
+    semver.gte(normalizedVersion, HYDROGEN_MIN_VERSION);
+
+  const success = h2Version === 'latest' || latestHydrogen;
+
+  const description = success
+    ? `Has latest hydrogen version`
+    : `Didn’t find latest version of hydrogen (${HYDROGEN_MIN_VERSION}), found ${normalizedVersion})`;
 
   return [
     {
       id: 'hydrogen-latest',
       type: 'Dependencies',
-      description: 'Has latest hydrogen',
-      success:
-        h2Version === 'latest' ||
-        (typeof normalizedVersion === 'string' &&
-          semver.gt(normalizedVersion, HYDROGEN_MIN_VERSION)),
-      fix: async (env) => {
-        await addHydrogen(env);
-      },
+      description,
+      success,
+      fix: addHydrogen,
     },
   ];
 }
