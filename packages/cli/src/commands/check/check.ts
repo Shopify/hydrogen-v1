5bbb2979192cc0d1bb407d24bf66c5fcd73a6ad5
Matt Seccafien <mseccafien@gmail.com>
2022-01-07 00:26:21 +0100

    fix: limit checks for now

--- packages/cli/src/commands/check/check.ts
+++ packages/cli/src/commands/check/check.ts
@@ -4,7 +4,6 @@ import {
   checkHydrogenVersion,
   checkEslintConfig,
   checkNodeVersion,
-  checkShopify,
 } from './rules';
 
 export async function check(env: Env) {
@@ -19,7 +18,6 @@ export async function check(env: Env) {
       ...(await checkNodeVersion(env)),
       ...(await checkHydrogenVersion(env)),
       ...(await checkEslintConfig(env)),
-      ...(await checkShopify(env)),
     ];
   } catch (error) {
     console.log(error);
