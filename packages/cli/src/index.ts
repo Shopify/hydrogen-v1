c0c6e9330fbb27d0c3ba59b504e64340f56f5cd9
Matt Seccafien <mseccafien@gmail.com>
2022-01-07 05:46:14 +0100

    feat: Support local commands in CLI

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,62 +1,38 @@
 import debug from 'debug';
-import {parseCliArguments, InputError, UnknownError} from './utilities';
+
+import {parseCliArguments, InputError} from './utilities';
 import {Cli} from './ui';
 import {Workspace} from './workspace';
 import {Fs} from './fs';
 import {loadConfig} from './config';
-import {Env} from './types';
+import {Command} from './Command';
 
 const logger = debug('hydrogen');
 
-interface ModuleType {
-  default: (env: Env) => Promise<void>;
-}
-
 (async () => {
   const rawInputs = process.argv.slice(2);
-  const {command, root} = parseCliArguments(rawInputs);
+  const {root, ...inputs} = parseCliArguments(rawInputs);
   const ui = new Cli();
-
   const config = (await loadConfig('hydrogen', {root})) || {};
   const workspace = new Workspace({root, ...config});
   const fs = new Fs(root);
 
-  if (!command) {
+  if (!inputs.command) {
     ui.say(`Missing command input`, {error: true});
 
     throw new InputError();
   }
 
-  async function runModule(module: ModuleType) {
-    logger('Run start');
+  const command = new Command(inputs.command);
 
-    await module.default({
-      ui,
-      fs,
-      workspace,
-      logger: debug(`hydrogen/${command}`),
-    });
-
-    logger('Run end');
-  }
-
-  try {
-    const module = await import(`./commands/${command}`);
-
-    await runModule(module);
-  } catch (error) {
-    logger(error);
-
-    throw new UnknownError();
-  }
+  await command.load();
+  await command.run({ui, fs, workspace, logger});
 
   for await (const file of fs.commit()) {
     ui.printFile(file);
   }
 
-  if (command === 'init') {
-    await workspace.commit();
-  }
+  await workspace.commit();
 })().catch((error) => {
   logger(error);
   process.exitCode = 1;
