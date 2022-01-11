8650188bf04bc3a2c5be03b44bc18d46ebd3f9b9
Fran Dios <fran.dios@shopify.com>
2022-01-11 12:19:12 +0100

    feat: remove old dependencies

--- packages/hydrogen/src/framework/plugins/vite-plugin-hydrogen-config.ts
+++ packages/hydrogen/src/framework/plugins/vite-plugin-hydrogen-config.ts
@@ -6,14 +6,6 @@ export default () => {
     config: async (config, env) => ({
       resolve: {
         alias: {
-          /**
-           * For some reason, when building in a worker, Vite always
-           * pulls the client version instead of the server version of these
-           * dependencies. We intentially force it to load the server versions.
-           */
-          'html-dom-parser': process.env.WORKER
-            ? 'html-dom-parser/lib/server/html-to-dom'
-            : 'html-dom-parser',
           // This library is currently included as a compiled vendor lib, not published yet to NPM
           'react-server-dom-vite/client-proxy': require.resolve(
             '@shopify/hydrogen/vendor/react-server-dom-vite/esm/react-server-dom-vite-client-proxy.js'
@@ -61,8 +53,6 @@ export default () => {
            * correct version of the dependency (server vs client). This tells Vite to take the
            * server versions and optimize them for ESM.
            */
-          'html-dom-parser',
-          'html-react-parser',
           'react-helmet-async',
           /**
            * Vite cannot find the following dependencies since they might be
