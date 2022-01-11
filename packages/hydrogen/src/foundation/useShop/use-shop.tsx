82a7b8e9a61dfd05c2e16d2ff68ce17356e7c354
aslemammad <zorofight94@gmail.com>
2022-01-12 01:20:47 +0330

    feat: add null value handling

--- packages/hydrogen/src/foundation/useShop/use-shop.tsx
+++ packages/hydrogen/src/foundation/useShop/use-shop.tsx
@@ -11,6 +11,15 @@ export function useShop(): ShopifyProviderValue {
   if (!context) {
     throw new Error('No Shopify Context found');
   }
+  if (!context.storeDomain) {
+    throw new Error('`storeDomain` should be defined in `shopify.config.js`');
+  }
+
+  if (!context.storefrontToken) {
+    throw new Error(
+      '`storefrontToken` should be defined in `shopify.config.js`'
+    );
+  }
 
   return context;
 }
