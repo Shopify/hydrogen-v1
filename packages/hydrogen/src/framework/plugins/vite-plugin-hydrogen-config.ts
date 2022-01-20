1474e1562cf736e97042d6391b4b81d4cb9e264e
Fran Dios <fran.dios@shopify.com>
2022-01-20 20:56:01 +0900

    fix: add another temporary monkey patch for React Fizz/Flight in workers build

--- packages/hydrogen/src/framework/plugins/vite-plugin-hydrogen-config.ts
+++ packages/hydrogen/src/framework/plugins/vite-plugin-hydrogen-config.ts
@@ -82,11 +82,24 @@ export default () => {
           const workerBundle = bundle[bundleKey];
           // It's always a chunk, this is just for TypeScript
           if (workerBundle.type === 'chunk') {
-            // Code tries to access an undefined value. This
-            // puts a guard before accessing it.
+            // React fizz and flight try to access an undefined value.
+            // This puts a guard before accessing it.
             workerBundle.code = workerBundle.code.replace(
-              /\(([a-z0-9]+)\.locked\)/gm,
+              /\((\w+)\.locked\)/gm,
               '($1 && $1.locked)'
+            );
+
+            // `renderToReadableStream` is bugged in React.
+            // This adds a workaround until these issues are fixed:
+            // https://github.com/facebook/react/issues/22772
+            // https://github.com/facebook/react/issues/23113
+            workerBundle.code = workerBundle.code.replace(
+              /var \w+\s*=\s*(\w+)\.completedRootSegment;/g,
+              'if($1.status===5)return\n$1.status=5;\n$&'
+            );
+            workerBundle.code = workerBundle.code.replace(
+              /(\w+)\.allPendingTasks\s*={2,3}\s*0\s*\&\&\s*\w+\.pingedTasks\.length/g,
+              '$1.status=0;\n$&'
             );
           }
         }
