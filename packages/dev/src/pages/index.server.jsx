4c95e024f1cfa251215a6a203046f49eabb45c67
Josh Larson <josh.larson@shopify.com>
2022-01-20 09:19:07 -0600

    fix: Fix hydration during dev by using more Suspense boundaries

--- packages/dev/src/pages/index.server.jsx
+++ packages/dev/src/pages/index.server.jsx
@@ -11,6 +11,94 @@ import Layout from '../components/Layout.server';
 import FeaturedCollection from '../components/FeaturedCollection';
 import ProductCard from '../components/ProductCard';
 import Welcome from '../components/Welcome.server';
+import {Suspense} from 'react';
+
+export default function Index({country = {isoCode: 'US'}}) {
+  return (
+    <Layout hero={<GradientBackground />}>
+      <div className="relative mb-12">
+        <Welcome />
+        <Suspense fallback={<BoxFallback />}>
+          <FeaturedProductsBox country={country} />
+        </Suspense>
+        <Suspense fallback={<BoxFallback />}>
+          <FeaturedCollectionBox country={country} />
+        </Suspense>
+      </div>
+    </Layout>
+  );
+}
+
+function BoxFallback() {
+  return <div className="bg-white p-12 shadow-xl rounded-xl mb-10 h-40"></div>;
+}
+
+function FeaturedProductsBox({country}) {
+  const {data} = useShopQuery({
+    query: QUERY,
+    variables: {
+      country: country.isoCode,
+    },
+  });
+
+  const collections = data ? flattenConnection(data.collections) : [];
+  const featuredProductsCollection = collections[0];
+  const featuredProducts = featuredProductsCollection
+    ? flattenConnection(featuredProductsCollection.products)
+    : null;
+
+  return (
+    <div className="bg-white p-12 shadow-xl rounded-xl mb-10">
+      {featuredProductsCollection ? (
+        <>
+          <div className="flex justify-between items-center mb-8 text-md font-medium">
+            <span className="text-black uppercase">
+              {featuredProductsCollection.title}
+            </span>
+            <span className="hidden md:inline-flex">
+              <Link
+                to={`/collections/${featuredProductsCollection.handle}`}
+                className="text-blue-600 hover:underline"
+              >
+                Shop all
+              </Link>
+            </span>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
+            {featuredProducts.map((product) => (
+              <div key={product.id}>
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
+          <div className="md:hidden text-center">
+            <Link
+              to={`/collections/${featuredProductsCollection.handle}`}
+              className="text-blue-600"
+            >
+              Shop all
+            </Link>
+          </div>
+        </>
+      ) : null}
+    </div>
+  );
+}
+
+function FeaturedCollectionBox({country}) {
+  const {data} = useShopQuery({
+    query: QUERY,
+    variables: {
+      country: country.isoCode,
+    },
+  });
+
+  const collections = data ? flattenConnection(data.collections) : [];
+  const featuredCollection =
+    collections && collections.length > 1 ? collections[1] : collections[0];
+
+  return <FeaturedCollection collection={featuredCollection} />;
+}
 
 function GradientBackground() {
   return (
@@ -64,66 +152,6 @@ function GradientBackground() {
         </defs>
       </svg>
     </div>
-  );
-}
-
-export default function Index({country = {isoCode: 'US'}}) {
-  const {data} = useShopQuery({
-    query: QUERY,
-    variables: {
-      country: country.isoCode,
-    },
-  });
-
-  const collections = data ? flattenConnection(data.collections) : [];
-  const featuredProductsCollection = collections[0];
-  const featuredProducts = featuredProductsCollection
-    ? flattenConnection(featuredProductsCollection.products)
-    : null;
-  const featuredCollection =
-    collections && collections.length > 1 ? collections[1] : collections[0];
-
-  return (
-    <Layout hero={<GradientBackground />}>
-      <div className="relative mb-12">
-        <Welcome />
-        <div className="bg-white p-12 shadow-xl rounded-xl mb-10">
-          {featuredProductsCollection ? (
-            <>
-              <div className="flex justify-between items-center mb-8 text-md font-medium">
-                <span className="text-black uppercase">
-                  {featuredProductsCollection.title}
-                </span>
-                <span className="hidden md:inline-flex">
-                  <Link
-                    to={`/collections/${featuredProductsCollection.handle}`}
-                    className="text-blue-600 hover:underline"
-                  >
-                    Shop all
-                  </Link>
-                </span>
-              </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
-                {featuredProducts.map((product) => (
-                  <div key={product.id}>
-                    <ProductCard product={product} />
-                  </div>
-                ))}
-              </div>
-              <div className="md:hidden text-center">
-                <Link
-                  to={`/collections/${featuredCollection.handle}`}
-                  className="text-blue-600"
-                >
-                  Shop all
-                </Link>
-              </div>
-            </>
-          ) : null}
-        </div>
-        <FeaturedCollection collection={featuredCollection} />
-      </div>
-    </Layout>
   );
 }
 
