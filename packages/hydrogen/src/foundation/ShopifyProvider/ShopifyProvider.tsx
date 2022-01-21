4ce2c7114e94f92dcfca2c3a45e3ee23d0bd8f46
Josh Larson <josh.larson@shopify.com>
2022-01-21 10:19:00 -0600

    chore: update docs

--- packages/hydrogen/src/foundation/ShopifyProvider/ShopifyProvider.tsx
+++ packages/hydrogen/src/foundation/ShopifyProvider/ShopifyProvider.tsx
@@ -6,6 +6,8 @@ import {DEFAULT_API_VERSION} from '../constants';
 /**
  * The `ShopifyProvider` component wraps your entire app and provides support for hooks.
  * You should place it in your app's entry point component. For example, `<App>`.
+ * If you are using the Hydrogen framework, you do not need to add this provider,
+ * since it's automatically wrapper around your app in `renderHydrogen()`.
  */
 export function ShopifyProvider({
   shopifyConfig,
