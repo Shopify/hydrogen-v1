50851f25b410bc4a05ee5eaa414c77dc15a8c4fb
Fran Dios <fran.dios@shopify.com>
2022-02-03 15:20:07 +0900

    fix: tree shake server logic in ShopifyProvider for browser

--- packages/hydrogen/src/foundation/useShop/use-shop.tsx
+++ packages/hydrogen/src/foundation/useShop/use-shop.tsx
@@ -1,31 +1,16 @@
 import {useContext} from 'react';
 import {ShopifyContext} from '../ShopifyProvider';
-import type {ShopifyContextValue} from '../ShopifyProvider/types';
-import {
-  NoServerRequestContext,
-  useServerRequest,
-} from '../ServerRequestProvider';
+import {useServerRequest} from '../ServerRequestProvider';
 
 // let contextValue: ShopifyContextValue | null = null;
 
 /**
  * The `useShop` hook provides access to values within `shopify.config.js`. It must be a descendent of a `ShopifyProvider` component.
  */
-export function useShop(): ShopifyContextValue {
-  let config: ShopifyContextValue | null = null;
-
-  try {
-    const request = useServerRequest();
-    config = request?.ctx?.shopifyConfig;
-  } catch (e) {
-    if (!(e instanceof NoServerRequestContext)) {
-      throw e;
-    }
-  }
-
-  if (!config) {
-    config = useContext(ShopifyContext);
-  }
+export function useShop() {
+  const config = import.meta.env.SSR
+    ? useServerRequest().ctx.shopifyConfig
+    : useContext(ShopifyContext);
 
   if (!config) {
     throw new Error('No Shopify Context found');
