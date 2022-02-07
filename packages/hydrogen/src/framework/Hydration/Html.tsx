07cb294ce51f49c2e2eb5ae438c54f62fc5ed939
Ryan Keller <ryan.keller@shopify.com>
2022-02-07 12:09:15 -0600

    chore: note about change needed

--- packages/hydrogen/src/framework/Hydration/Html.tsx
+++ packages/hydrogen/src/framework/Hydration/Html.tsx
@@ -11,6 +11,7 @@ type HtmlOptions = {
 export function Html({children, template, htmlAttrs, bodyAttrs}: HtmlOptions) {
   const head = template.match(/<head>(.+?)<\/head>/s)![1] || '';
 
+  // TODO: find a way to reference vite config or shopify config, use to set the script src dir
   return (
     <html {...htmlAttrs}>
       <head dangerouslySetInnerHTML={{__html: head}} />
