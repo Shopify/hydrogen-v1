3f07968b0b51e038d6847914a6a2d7b527dca571
Michelle Chen <michelle.chen@shopify.com>
2022-02-09 15:32:47 -0500

    refactor: move the rendering to one place

--- examples/template-hydrogen-default/src/pages/sitemap.xml.server.jsx
+++ examples/template-hydrogen-default/src/pages/sitemap.xml.server.jsx
@@ -21,68 +21,90 @@ export default function Sitemap({response}) {
 function shopSitemap(data) {
   const urlOrigin = 'https://hydrogen-preview.myshopify.com';
 
+  const productsData = flattenConnection(data.products).map((product) => {
+    const url = product.onlineStoreUrl
+      ? product.onlineStoreUrl
+      : `${urlOrigin}/products/${product.handle}`;
+
+    const finalObject = {
+      url,
+      lastMod: product.updatedAt,
+      changeFreq: 'daily',
+    };
+
+    if (product.featuredImage.url) {
+      finalObject.image = {
+        url: product.featuredImage.url,
+      };
+
+      if (product.title) {
+        finalObject.image.title = product.title;
+      }
+
+      if (product.featuredImage.altText) {
+        finalObject.image.caption = product.featuredImage.altText;
+      }
+
+      return finalObject;
+    }
+  });
+
+  const collectionsData = flattenConnection(data.collections).map(
+    (collection) => {
+      const url = collection.onlineStoreUrl
+        ? collection.onlineStoreUrl
+        : `${urlOrigin}/collections/${collection.handle}`;
+
+      return {
+        url,
+        lastMod: collection.updatedAt,
+        changeFreq: 'daily',
+      };
+    },
+  );
+
+  const pagesData = flattenConnection(data.pages).map((page) => {
+    const url = page.onlineStoreUrl
+      ? page.onlineStoreUrl
+      : `${urlOrigin}/pages/${page.handle}`;
+
+    return {
+      url,
+      lastMod: page.updatedAt,
+      changeFreq: 'weekly',
+    };
+  });
+
+  const urlsDatas = [...productsData, ...collectionsData, ...pagesData];
+
   return `
     <urlset
       xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
       xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
     >
-      ${flattenConnection(data.products)
-        .map((product) => {
-          const url = product.onlineStoreUrl
-            ? product.onlineStoreUrl
-            : `${urlOrigin}/products/${product.handle}`;
-
-          return `
-          <url>
-            <loc>${url}</loc>
-            <lastmod>${product.updatedAt}</lastmod>
-            <changefreq>daily</changefreq>
-            <image:image>
-              <image:loc>
-                ${product?.featuredImage?.url}
-              </image:loc>
-              <image:title>
-                ${product?.title ?? ''}
-              </image:title>
-              <image:caption>
-                ${product?.featuredImage?.altText ?? ''}
-              </image:caption>
-            </image:image>
-          </url>
-        `;
-        })
-        .join('')}
-      ${flattenConnection(data.collections)
-        .map((collection) => {
-          const url = collection.onlineStoreUrl
-            ? collection.onlineStoreUrl
-            : `${urlOrigin}/collections/${collection.handle}`;
-
-          return `
-          <url>
-            <loc>${url}</loc>
-            <lastmod>${collection.updatedAt}</lastmod>
-            <changefreq>daily</changefreq>
-          </url>
-        `;
-        })
-        .join('')}
-      ${flattenConnection(data.pages)
-        .map((page) => {
-          const url = page.onlineStoreUrl
-            ? page.onlineStoreUrl
-            : `${urlOrigin}/pages/${page.handle}`;
-
-          return `
-          <url>
-            <loc>${url}</loc>
-            <lastmod>${page.updatedAt}</lastmod>
-            <changefreq>weekly</changefreq>
-          </url>
-        `;
-        })
-        .join('')}
+      ${urlsDatas.map((url) => renderUrlTag(url)).join('')}
     </urlset>`;
+}
+
+function renderUrlTag({url, lastMod, changeFreq, image}) {
+  return `
+    <url>
+      <loc>${url}</loc>
+      <lastmod>${lastMod}</lastmod>
+      <changefreq>${changeFreq}</changefreq>
+      ${
+        image
+          ? `
+        <image:image>
+          <image:loc>${image.url}</image:loc>
+          <image:title>${image.title ?? ''}</image:title>
+          <image:caption>${image.caption ?? ''}</image:caption>
+        </image:image>`
+          : ''
+      }
+      
+    </url>
+  `;
 }
 
 const QUERY = gql`
