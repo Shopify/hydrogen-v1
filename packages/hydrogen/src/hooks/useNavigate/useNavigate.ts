2e639c6857b6d16e13be1640d2a60ff4d6792fb2
Bret Little <bret.little@shopify.com>
2022-02-14 11:58:38 -0500

    docs: more docs fixes

--- packages/hydrogen/src/hooks/useNavigate/useNavigate.ts
+++ packages/hydrogen/src/hooks/useNavigate/useNavigate.ts
@@ -1,8 +1,13 @@
 import {useRouter} from '../../foundation/Router/Router.client';
 
 type NavigationOptions = {
+  /** Whether to update the state object or URL of the current history entry. Default to false */
   replace?: boolean;
+
+  /** Whether to reload the whole document on navigation */
   reloadDocument?: boolean;
+
+  /** The custom client state with the navigation */
   clientState?: any;
 };
 
