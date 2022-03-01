f55fb12cb0cb748e3e1555a2a5ab5de417a5153c
Helen Lin <helen.lin@shopify.com>
2022-03-01 12:05:29 -0800

    move Route codes from bl-router-refactor

--- packages/hydrogen/src/foundation/Router/Router.server.tsx
+++ packages/hydrogen/src/foundation/Router/Router.server.tsx
@@ -38,6 +38,47 @@ function recurseChildren(
           </>
         );
     }
+
+    if (child.type === Route) {
+      const match = matchPath(currentPath, {
+        path: child.props.path,
+        exact: true,
+      });
+
+      if (match) {
+        request.ctx.routeParams = match.params;
+        debugger;
+        return (
+          <>
+            {cloneElement(child.props.page, {
+              params: match.useParams,
+              ...serverProps,
+            })}
+            <Boomerang
+              pageTemplate={child.props.page?.name ?? child.props.page}
+            />
+          </>
+        );
+      }
+    } else if (child.props.children) {
+      return recurseChildren(
+        request,
+        serverProps,
+        fallback,
+        currentPath,
+        child.props.children
+      );
+    } else {
+      if (typeof child.type === 'function') {
+        return recurseChildren(
+          request,
+          serverProps,
+          fallback,
+          currentPath,
+          (child.type as any)(child.props)
+        );
+      }
+    }
   }
 
   const fallbackName = fallback.type.toString().match(/function ([^\(]+)/);
---
+++ packages/hydrogen/src/foundation/Router/tests/Router.test.tsx
@@ -0,0 +1,133 @@
+import {IncomingMessage} from 'http';
+import {mount} from '@shopify/react-testing';
+import React, {ReactElement} from 'react';
+import {Router} from '../Router.server';
+import {Route} from '../Route.server';
+import {ServerRequestProvider} from '../../ServerRequestProvider';
+import {useParams} from '../useParams';
+import {ServerComponentRequest} from '../../../framework/Hydration/ServerComponentRequest.server';
+
+jest.mock('../../Boomerang/Boomerang.client', () => ({
+  Boomerang: () => null,
+}));
+
+function renderRoutes(
+  url: string,
+  routes: Array<ReactElement> | ReactElement,
+  serverProps = {}
+) {
+  //@ts-ignore
+  const nodeRequest = new IncomingMessage();
+  nodeRequest.url = url;
+  const request = new ServerComponentRequest(nodeRequest);
+  return mount(
+    <ServerRequestProvider request={request} isRSC={false}>
+      <Router fallback={<h1>Not found!</h1>} serverProps={serverProps}>
+        {routes}
+      </Router>
+    </ServerRequestProvider>
+  );
+}
+
+describe('Router', () => {
+  it('Matches a root <Route>', () => {
+    const wrapper = renderRoutes(
+      '/',
+      <Route path="/" page={<h1>Root Route</h1>} />
+    );
+
+    expect(wrapper.html()).toMatchInlineSnapshot(`"<h1>Root Route</h1>"`);
+  });
+
+  it('Only matches one route', () => {
+    const wrapper = renderRoutes(
+      '/',
+      <>
+        <Route path="/" page={<h1>Root Route</h1>} />
+        <Route path="/" page={<h1>Root Route 2</h1>} />
+      </>
+    );
+
+    expect(wrapper.html()).toMatchInlineSnapshot(`"<h1>Root Route</h1>"`);
+  });
+
+  it('Only matches last route', () => {
+    const wrapper = renderRoutes(
+      '/last',
+      <>
+        <Route path="/" page={<h1>Root Route</h1>} />
+        <Route path="/last" page={<h1>Last Route</h1>} />
+      </>
+    );
+
+    expect(wrapper.html()).toMatchInlineSnapshot(`"<h1>Last Route</h1>"`);
+  });
+
+  it('Matches routes in nested components', () => {
+    function A() {
+      return <B />;
+    }
+    function B() {
+      return <Route path="/nested" page={<h1>Nested Route</h1>} />;
+    }
+    const wrapper = renderRoutes(
+      '/nested',
+      <>
+        <Route path="/" page={<h1>Root Route</h1>} />
+        <A />
+      </>
+    );
+
+    expect(wrapper.html()).toMatchInlineSnapshot(`"<h1>Nested Route</h1>"`);
+  });
+
+  it('Matches routes with params', () => {
+    const wrapper = renderRoutes(
+      '/products/hydrogen',
+      <Route path="/products/:handle" page={<h1>Hydrogen Route</h1>} />
+    );
+
+    expect(wrapper.html()).toMatchInlineSnapshot(`"<h1>Hydrogen Route</h1>"`);
+  });
+
+  it('Routes have access to route params', () => {
+    function MyRoute() {
+      const {handle} = useParams();
+      return <h1>{handle}</h1>;
+    }
+
+    const wrapper = renderRoutes(
+      '/products/hydrogen',
+      <Route path="/products/:handle" page={<MyRoute />} />
+    );
+
+    expect(wrapper.html()).toMatchInlineSnapshot(`"<h1>hydrogen</h1>"`);
+  });
+
+  it('Renders fallback when no routes match', () => {
+    const wrapper = renderRoutes(
+      '/does-note-exist',
+      // @ts-ignore
+      <Route path="/" page={<h1>Should not show up</h1>} />
+    );
+
+    expect(wrapper.html()).toMatchInlineSnapshot(`"<h1>Not found!</h1>"`);
+  });
+
+  it('Routes get server state', () => {
+    function MyRoute({someState}: {someState: string}) {
+      return <h1>{someState}</h1>;
+    }
+
+    const wrapper = renderRoutes(
+      '/products/hydrogen',
+      // @ts-ignore
+      <Route path="/products/:handle" page={<MyRoute />} />,
+      {someState: 'some server state'}
+    );
+
+    expect(wrapper.html()).toMatchInlineSnapshot(
+      `"<h1>some server state</h1>"`
+    );
+  });
+});
