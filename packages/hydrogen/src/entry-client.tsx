7b0d2cbd6409320dafb5142b9ba71a01168ed5ff
Fran Dios <fran.dios@shopify.com>
2022-03-07 17:35:12 +0900

    Change ServerStateProvider to be a shared component and avoid extra bundling in client build

--- packages/hydrogen/src/entry-client.tsx
+++ packages/hydrogen/src/entry-client.tsx
@@ -10,7 +10,7 @@ import {hydrateRoot} from 'react-dom';
 import type {ClientHandler} from './types';
 import {ErrorBoundary} from 'react-error-boundary';
 import {useServerResponse} from './framework/Hydration/rsc';
-import {ServerStateProvider} from './client';
+import {ServerStateProvider} from './foundation/ServerStateProvider';
 import {Router} from './foundation/Router/Router';
 
 const renderHydrogen: ClientHandler = async (ClientWrapper, config) => {
--- packages/hydrogen/src/foundation/ServerStateProvider/index.ts
+++ packages/hydrogen/src/foundation/ServerStateProvider/index.ts
@@ -1,8 +1,2 @@
-export {
-  ServerStateProvider,
-  ServerStateContext,
-} from './ServerStateProvider.client';
-export type {
-  ServerState,
-  ServerStateContextValue,
-} from './ServerStateProvider.client';
+export {ServerStateProvider, ServerStateContext} from './ServerStateProvider';
+export type {ServerState, ServerStateContextValue} from './ServerStateProvider';
