c2cea5fa10eede1777e9410c4daa94dc36ac3b51
Anthony Frehner <frehner@users.noreply.github.com>
2022-03-14 11:55:55 -0600

    VariantFragment and ProductFragment removal (#899)

--- examples/template-hydrogen-default/src/routes/collections/[handle].server.jsx
+++ examples/template-hydrogen-default/src/routes/collections/[handle].server.jsx
@@ -1,5 +1,4 @@
 import {useShopQuery, flattenConnection, Seo} from '@shopify/hydrogen';
-import {ProductProviderFragment} from '@shopify/hydrogen/fragments';
 import gql from 'graphql-tag';
 
 import LoadMoreProducts from '../../components/LoadMoreProducts.client';
@@ -93,7 +92,273 @@ const QUERY = gql`
         edges {
           node {
             vendor
-            ...ProductProviderFragment
+            compareAtPriceRange {
+              maxVariantPrice {
+                currencyCode
+                amount
+              }
+              minVariantPrice {
+                currencyCode
+                amount
+              }
+            }
+            descriptionHtml
+            handle
+            id
+            media(first: $numProductMedia) {
+              edges {
+                node {
+                  ... on MediaImage {
+                    mediaContentType
+                    image {
+                      id
+                      url
+                      altText
+                      width
+                      height
+                    }
+                  }
+                  ... on Video {
+                    mediaContentType
+                    id
+                    previewImage {
+                      url
+                    }
+                    sources {
+                      mimeType
+                      url
+                    }
+                  }
+                  ... on ExternalVideo {
+                    mediaContentType
+                    id
+                    embedUrl
+                    host
+                  }
+                  ... on Model3d {
+                    mediaContentType
+                    id
+                    alt
+                    mediaContentType
+                    previewImage {
+                      url
+                    }
+                    sources {
+                      url
+                    }
+                  }
+                }
+              }
+            }
+            metafields(first: $numProductMetafields) {
+              edges {
+                node {
+                  id
+                  type
+                  namespace
+                  key
+                  value
+                  createdAt
+                  updatedAt
+                  description
+                  reference @include(if: $includeReferenceMetafieldDetails) {
+                    __typename
+                    ... on MediaImage {
+                      id
+                      mediaContentType
+                      image {
+                        id
+                        url
+                        altText
+                        width
+                        height
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            priceRange {
+              maxVariantPrice {
+                currencyCode
+                amount
+              }
+              minVariantPrice {
+                currencyCode
+                amount
+              }
+            }
+            title
+            variants(first: $numProductVariants) {
+              edges {
+                node {
+                  id
+                  title
+                  availableForSale
+                  image {
+                    id
+                    url
+                    altText
+                    width
+                    height
+                  }
+                  unitPriceMeasurement {
+                    measuredType
+                    quantityUnit
+                    quantityValue
+                    referenceUnit
+                    referenceValue
+                  }
+                  unitPrice {
+                    currencyCode
+                    amount
+                  }
+                  priceV2 {
+                    currencyCode
+                    amount
+                  }
+                  compareAtPriceV2 {
+                    currencyCode
+                    amount
+                  }
+                  selectedOptions {
+                    name
+                    value
+                  }
+                  metafields(first: $numProductVariantMetafields) {
+                    edges {
+                      node {
+                        id
+                        type
+                        namespace
+                        key
+                        value
+                        createdAt
+                        updatedAt
+                        description
+                        reference
+                          @include(if: $includeReferenceMetafieldDetails) {
+                          __typename
+                          ... on MediaImage {
+                            id
+                            mediaContentType
+                            image {
+                              id
+                              url
+                              altText
+                              width
+                              height
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                  sellingPlanAllocations(
+                    first: $numProductVariantSellingPlanAllocations
+                  ) {
+                    edges {
+                      node {
+                        priceAdjustments {
+                          compareAtPrice {
+                            currencyCode
+                            amount
+                          }
+                          perDeliveryPrice {
+                            currencyCode
+                            amount
+                          }
+                          price {
+                            currencyCode
+                            amount
+                          }
+                          unitPrice {
+                            currencyCode
+                            amount
+                          }
+                        }
+                        sellingPlan {
+                          id
+                          description
+                          name
+                          options {
+                            name
+                            value
+                          }
+                          priceAdjustments {
+                            orderCount
+                            adjustmentValue {
+                              ... on SellingPlanFixedAmountPriceAdjustment {
+                                adjustmentAmount {
+                                  currencyCode
+                                  amount
+                                }
+                              }
+                              ... on SellingPlanFixedPriceAdjustment {
+                                price {
+                                  currencyCode
+                                  amount
+                                }
+                              }
+                              ... on SellingPlanPercentagePriceAdjustment {
+                                adjustmentPercentage
+                              }
+                            }
+                          }
+                          recurringDeliveries
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            sellingPlanGroups(first: $numProductSellingPlanGroups) {
+              edges {
+                node {
+                  sellingPlans(first: $numProductSellingPlans) {
+                    edges {
+                      node {
+                        id
+                        description
+                        name
+                        options {
+                          name
+                          value
+                        }
+                        priceAdjustments {
+                          orderCount
+                          adjustmentValue {
+                            ... on SellingPlanFixedAmountPriceAdjustment {
+                              adjustmentAmount {
+                                currencyCode
+                                amount
+                              }
+                            }
+                            ... on SellingPlanFixedPriceAdjustment {
+                              price {
+                                currencyCode
+                                amount
+                              }
+                            }
+                            ... on SellingPlanPercentagePriceAdjustment {
+                              adjustmentPercentage
+                            }
+                          }
+                        }
+                        recurringDeliveries
+                      }
+                    }
+                  }
+                  appName
+                  name
+                  options {
+                    name
+                    values
+                  }
+                }
+              }
+            }
           }
         }
         pageInfo {
@@ -102,6 +367,4 @@ const QUERY = gql`
       }
     }
   }
-
-  ${ProductProviderFragment}
 `;
--- examples/template-hydrogen-default/src/routes/index.server.jsx
+++ examples/template-hydrogen-default/src/routes/index.server.jsx
@@ -5,7 +5,6 @@ import {
   Seo,
   CacheDays,
 } from '@shopify/hydrogen';
-import {ProductProviderFragment} from '@shopify/hydrogen/fragments';
 import gql from 'graphql-tag';
 
 import Layout from '../components/Layout.server';
@@ -224,7 +223,274 @@ const QUERY = gql`
           products(first: $numProducts) {
             edges {
               node {
-                ...ProductProviderFragment
+                compareAtPriceRange {
+                  maxVariantPrice {
+                    currencyCode
+                    amount
+                  }
+                  minVariantPrice {
+                    currencyCode
+                    amount
+                  }
+                }
+                descriptionHtml
+                handle
+                id
+                media(first: $numProductMedia) {
+                  edges {
+                    node {
+                      ... on MediaImage {
+                        mediaContentType
+                        image {
+                          id
+                          url
+                          altText
+                          width
+                          height
+                        }
+                      }
+                      ... on Video {
+                        mediaContentType
+                        id
+                        previewImage {
+                          url
+                        }
+                        sources {
+                          mimeType
+                          url
+                        }
+                      }
+                      ... on ExternalVideo {
+                        mediaContentType
+                        id
+                        embedUrl
+                        host
+                      }
+                      ... on Model3d {
+                        mediaContentType
+                        id
+                        alt
+                        mediaContentType
+                        previewImage {
+                          url
+                        }
+                        sources {
+                          url
+                        }
+                      }
+                    }
+                  }
+                }
+                metafields(first: $numProductMetafields) {
+                  edges {
+                    node {
+                      id
+                      type
+                      namespace
+                      key
+                      value
+                      createdAt
+                      updatedAt
+                      description
+                      reference
+                        @include(if: $includeReferenceMetafieldDetails) {
+                        __typename
+                        ... on MediaImage {
+                          id
+                          mediaContentType
+                          image {
+                            id
+                            url
+                            altText
+                            width
+                            height
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+                priceRange {
+                  maxVariantPrice {
+                    currencyCode
+                    amount
+                  }
+                  minVariantPrice {
+                    currencyCode
+                    amount
+                  }
+                }
+                title
+                variants(first: $numProductVariants) {
+                  edges {
+                    node {
+                      id
+                      title
+                      availableForSale
+                      image {
+                        id
+                        url
+                        altText
+                        width
+                        height
+                      }
+                      unitPriceMeasurement {
+                        measuredType
+                        quantityUnit
+                        quantityValue
+                        referenceUnit
+                        referenceValue
+                      }
+                      unitPrice {
+                        currencyCode
+                        amount
+                      }
+                      priceV2 {
+                        currencyCode
+                        amount
+                      }
+                      compareAtPriceV2 {
+                        currencyCode
+                        amount
+                      }
+                      selectedOptions {
+                        name
+                        value
+                      }
+                      metafields(first: $numProductVariantMetafields) {
+                        edges {
+                          node {
+                            id
+                            type
+                            namespace
+                            key
+                            value
+                            createdAt
+                            updatedAt
+                            description
+                            reference
+                              @include(if: $includeReferenceMetafieldDetails) {
+                              __typename
+                              ... on MediaImage {
+                                id
+                                mediaContentType
+                                image {
+                                  id
+                                  url
+                                  altText
+                                  width
+                                  height
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                      sellingPlanAllocations(
+                        first: $numProductVariantSellingPlanAllocations
+                      ) {
+                        edges {
+                          node {
+                            priceAdjustments {
+                              compareAtPrice {
+                                currencyCode
+                                amount
+                              }
+                              perDeliveryPrice {
+                                currencyCode
+                                amount
+                              }
+                              price {
+                                currencyCode
+                                amount
+                              }
+                              unitPrice {
+                                currencyCode
+                                amount
+                              }
+                            }
+                            sellingPlan {
+                              id
+                              description
+                              name
+                              options {
+                                name
+                                value
+                              }
+                              priceAdjustments {
+                                orderCount
+                                adjustmentValue {
+                                  ... on SellingPlanFixedAmountPriceAdjustment {
+                                    adjustmentAmount {
+                                      currencyCode
+                                      amount
+                                    }
+                                  }
+                                  ... on SellingPlanFixedPriceAdjustment {
+                                    price {
+                                      currencyCode
+                                      amount
+                                    }
+                                  }
+                                  ... on SellingPlanPercentagePriceAdjustment {
+                                    adjustmentPercentage
+                                  }
+                                }
+                              }
+                              recurringDeliveries
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+                sellingPlanGroups(first: $numProductSellingPlanGroups) {
+                  edges {
+                    node {
+                      sellingPlans(first: $numProductSellingPlans) {
+                        edges {
+                          node {
+                            id
+                            description
+                            name
+                            options {
+                              name
+                              value
+                            }
+                            priceAdjustments {
+                              orderCount
+                              adjustmentValue {
+                                ... on SellingPlanFixedAmountPriceAdjustment {
+                                  adjustmentAmount {
+                                    currencyCode
+                                    amount
+                                  }
+                                }
+                                ... on SellingPlanFixedPriceAdjustment {
+                                  price {
+                                    currencyCode
+                                    amount
+                                  }
+                                }
+                                ... on SellingPlanPercentagePriceAdjustment {
+                                  adjustmentPercentage
+                                }
+                              }
+                            }
+                            recurringDeliveries
+                          }
+                        }
+                      }
+                      appName
+                      name
+                      options {
+                        name
+                        values
+                      }
+                    }
+                  }
+                }
               }
             }
           }
@@ -232,6 +498,4 @@ const QUERY = gql`
       }
     }
   }
-
-  ${ProductProviderFragment}
 `;
