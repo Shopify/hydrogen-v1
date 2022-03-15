a362a5dd02e94c7cdf62bb8d0d7e52e8676b415c
Matt Seccafien <matt.seccafien@shopify.com>
2022-03-15 13:16:39 +0100

    feat: add banned-hooks rules for client and server components (#877)
    * feat: add banned-hooks rules for client and server components
    
    BREAKING CHANGE
    
    * Disable build caching and don't continue on failure (#880)
    
    * Update packages/eslint-plugin/src/rules/client-component-banned-hooks/client-component-banned-hooks.ts
    
    Co-authored-by: Fran Dios <fran.dios@shopify.com>
    
    * Custom Router Implementation  (#846)
    
    Add custom router implementation. This mostly follows the RFC outlined here: #569
    
    There are mainly 3 areas modified:
    
    1. New <Route> component
    2. Changes (breaking) to <Router>
    3. Additive (non-breaking) changes to <FileRoutes>
    4. New useParams() hook that can be used in both server and client components
    
    * Remove useQuery hook from client exports (#870)
    
    * Remove useQuery hook from client exports
    
    * Update useQuery docs
    
    * Update docs
    
    * feat: add banned-hooks rules for client and server components
    
    BREAKING CHANGE
    
    * Update packages/eslint-plugin/src/rules/client-component-banned-hooks/client-component-banned-hooks.ts
    
    Co-authored-by: Fran Dios <fran.dios@shopify.com>
    
    * add changeset
    
    Co-authored-by: Josh Larson <josh.larson@shopify.com>
    Co-authored-by: Fran Dios <fran.dios@shopify.com>
    Co-authored-by: Bret Little <bret.little@shopify.com>

--- packages/eslint-plugin/src/rules/server-component-banned-hooks/tests/server-component-banned-hooks.test.ts
+++ packages/eslint-plugin/src/rules/server-component-banned-hooks/tests/server-component-banned-hooks.test.ts
@@ -1,6 +1,6 @@
 import {TSESLint} from '@typescript-eslint/experimental-utils';
 import {AST_NODE_TYPES} from '@typescript-eslint/types';
-import {noStateInServerComponents} from '../no-state-in-server-components';
+import {serverComponentBannedHooks} from '../server-component-banned-hooks';
 import dedent from 'dedent';
 
 const ruleTester = new TSESLint.RuleTester({
@@ -11,13 +11,13 @@ function error(hookName: string) {
   return {
     type: AST_NODE_TYPES.CallExpression,
     data: {hook: hookName},
-    messageId: 'noStateInServerComponents' as const,
+    messageId: 'serverComponentBannedHooks' as const,
   };
 }
 
 ruleTester.run(
-  'hydrogen/no-state-in-server-components',
-  noStateInServerComponents,
+  'hydrogen/server-component-banned-hooks',
+  serverComponentBannedHooks,
   {
     valid: [
       {
@@ -53,6 +53,41 @@ ruleTester.run(
           return null;
         }
       `,
+        filename: 'ServerComponent.server.tsx',
+      },
+      {
+        code: dedent`
+        function ClientComponent() {
+          useEffect(() => {});
+          return null;
+        }
+      `,
+        filename: 'ClientComponent.client.tsx',
+      },
+      {
+        code: dedent`
+          function ClientComponent() {
+            useLayoutEffect(() => {});
+            return null;
+          }
+        `,
+        filename: 'ClientComponent.client.tsx',
+      },
+      {
+        code: dedent`
+          function ServerComponent() {
+            return null;
+          }
+        `,
+        filename: 'ServerComponent.server.tsx',
+      },
+      {
+        code: dedent`
+          function ServerComponent() {
+            const {foo} = useBar();
+            return null;
+          }
+        `,
         filename: 'ServerComponent.server.tsx',
       },
     ],
@@ -117,6 +152,80 @@ ruleTester.run(
         }
       `,
         errors: [error('useReducer'), error('useState')],
+        filename: 'ServerComponent.tsx',
+      },
+      {
+        code: dedent`
+          function ServerComponent() {
+            useEffect(() => {});
+            return null;
+          }
+        `,
+        errors: [error('useEffect')],
+        filename: 'ServerComponent.server.tsx',
+      },
+      {
+        code: dedent`
+          function ServerComponent() {
+            useLayoutEffect(() => {});
+            return null;
+          }
+        `,
+        errors: [error('useLayoutEffect')],
+        filename: 'ServerComponent.server.tsx',
+      },
+      {
+        code: dedent`
+          function ServerComponent() {
+            useEffect(() => {});
+            useLayoutEffect(() => {});
+            return null;
+          }
+        `,
+        errors: [error('useEffect'), error('useLayoutEffect')],
+        filename: 'ServerComponent.server.tsx',
+      },
+      {
+        code: dedent`
+          function ServerComponent() {
+            useEffect(() => {});
+            return null;
+          }
+        `,
+        errors: [error('useEffect')],
+        filename: 'ServerComponent.tsx',
+      },
+      {
+        code: dedent`
+          function ServerComponent() {
+            useEffect(() => {});
+            return null;
+          }
+        `,
+        errors: [error('useEffect')],
+        filename: 'ServerComponent.tsx',
+      },
+      {
+        code: dedent`
+          function ServerComponent() {
+            useLayoutEffect(() => {});
+            useEffect(() => {});
+            return null;
+          }
+        `,
+        errors: [error('useLayoutEffect'), error('useEffect')],
+        filename: 'ServerComponent.tsx',
+      },
+
+      {
+        code: dedent`
+          function ServerComponent() {
+            React.useLayoutEffect(() => {});
+            React.useEffect(() => {});
+            return null;
+          }
+        `,
+        errors: [error('useLayoutEffect'), error('useEffect')],
         filename: 'ServerComponent.tsx',
       },
     ],
