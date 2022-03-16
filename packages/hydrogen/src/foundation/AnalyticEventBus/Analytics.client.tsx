5657cc60d903052bb19573b8aed68735351f85f1
Helen Lin <helen.lin@shopify.com>
2022-03-16 12:23:17 -0700

    remove window reference

--- packages/hydrogen/src/foundation/AnalyticEventBus/Analytics.client.tsx
+++ packages/hydrogen/src/foundation/AnalyticEventBus/Analytics.client.tsx
@@ -1,27 +1,11 @@
 import {useEffect} from 'react';
 import {ClientAnalytics} from './index';
 
-declare global {
-  interface Window {
-    Shopify: {
-      Analytics: any;
-    };
-  }
-}
-
 export function Analytics({
   analyticDataFromServer,
 }: {
   analyticDataFromServer: any;
 }) {
-  // Make AnalyticEventBus available in the client browser
-  // useEffect(() => {
-  //   (function () {
-  //     window.Shopify = window.Shopify || {};
-  //     window.Shopify.Analytics = window.Shopify.Analytics || ClientAnalytics;
-  //   })();
-  // }, [ClientAnalytics]);
-
   useEffect(() => {
     ClientAnalytics.pushToDatalayer(analyticDataFromServer);
     ClientAnalytics.publish('page-view', true);
