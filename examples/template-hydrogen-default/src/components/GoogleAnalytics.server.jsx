ecf30c209535f10411e6228b1a325781dde446e3
Helen Lin <helen.lin@shopify.com>
2022-03-18 15:05:08 -0700

    adjust to sending batch or single event .. still doesn't work

--- examples/template-hydrogen-default/src/components/GoogleAnalytics.server.jsx
+++ examples/template-hydrogen-default/src/components/GoogleAnalytics.server.jsx
@@ -1,15 +1,32 @@
-export function request(request, data) {
+export function request(request, data, contentType) {
   const url = new URL(request.url);
   if (url.pathname === '/__event/g/collect') {
-    fetch(
-      `https://analytics.google.com/g/collect${
-        url.search
-      }&uip=${request.headers.get('x-forwarded-for')}`,
-      {
-        headers: {
-          'user-agent': request.headers.get('user-agent'),
-        },
-      },
-    );
+    const gaUrl = `https://analytics.google.com/g/collect${
+      url.search
+    }&uip=${request.headers.get('x-forwarded-for')}`;
+    const headers = {
+      'User-Agent': request.headers.get('User-Agent'),
+      'Content-Type': request.headers.get('Content-Type')
+    };
+
+    try {
+      if (request.method === 'GET') {
+        fetch(gaUrl, {method: 'GET', headers})
+          .catch((err) => {
+            console.log(err);
+          });
+      } else {
+        fetch(gaUrl, {
+          method: 'POST',
+          headers,
+          body: contentType === 'json' ? JSON.stringify(data) : data
+        }).catch((err) => {
+          console.log(err);
+        });
+        ;
+      }
+    } catch(error) {
+      console.log(error);
+    }
   }
 }
