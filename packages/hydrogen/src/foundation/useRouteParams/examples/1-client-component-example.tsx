ca0c148a34be7ce6aaf057acc8a94296371b34de
Michelle Vinci <michelle.vinci@shopify.com>
2022-03-21 18:55:44 -0700

    remove example tsx files

--- packages/hydrogen/src/foundation/useRouteParams/examples/1-client-component-example.tsx
+++
@@ -1,8 +0,0 @@
-/** Client component example */
-
-import {useRouteParams} from '@shopify/hydrogen/client';
-// Client component
-export default function Component() {
-  const {handle} = useRouteParams();
-  return <h1>The handle route param is: {handle}</h1>;
-}
--- packages/hydrogen/src/foundation/useRouteParams/examples/2-server-component-example.tsx
+++
@@ -1,8 +0,0 @@
-/** Server component example */
-
-import {useRouteParams} from '@shopify/hydrogen';
-// Server component
-export default function Page() {
-  const {handle} = useRouteParams();
-  return <h1>The handle route param is: {handle}</h1>;
-}
