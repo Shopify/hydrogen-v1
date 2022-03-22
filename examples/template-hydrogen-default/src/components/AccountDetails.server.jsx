aeb8ba9d256d801a7ada09a3d6ea9c8a4fc5b11c
Michelle Chen <michelle.chen@shopify.com>
2022-03-22 15:29:53 -0400

    add checking to account details

--- examples/template-hydrogen-default/src/components/AccountDetails.server.jsx
+++ examples/template-hydrogen-default/src/components/AccountDetails.server.jsx
@@ -16,7 +16,9 @@ export default function AccountDetails({customerAccessToken}) {
   const customer = data && data.customer;
 
   const orders =
-    customer && customer.orders ? flattenConnection(customer.orders) : [];
+    customer && customer.orders && customer.orders.edges.length > 0
+      ? flattenConnection(customer.orders)
+      : [];
 
   const pageHeader = customer
     ? `Welcome ${customer.firstName || customer.email} !`
