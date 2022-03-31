18e59ee1a2b6be4afedbbe4efe5931362814174a
Helen Lin <helen.lin@shopify.com>
2022-03-31 16:33:57 -0700

    correct wrapping

--- examples/template-hydrogen-default/src/components/GTM.client.jsx
+++ examples/template-hydrogen-default/src/components/GTM.client.jsx
@@ -1,15 +1,22 @@
 import Analytics from 'analytics';
 import googleTagManager from '@analytics/google-tag-manager';
+import {useEffect} from 'react';
 
-Analytics({
-  app: 'hydrogen-app',
-  plugins: [
-    googleTagManager({
-      containerId: 'GTM-WLTS4QF',
-    }),
-  ],
-});
-
+let init = false;
 export default function GTM() {
+  useEffect(() => {
+    if (!init) {
+      // One time initialization
+      Analytics({
+        app: 'hydrogen-app',
+        plugins: [
+          googleTagManager({
+            containerId: 'GTM-WLTS4QF',
+          }),
+        ],
+      });
+      init = true;
+    }
+  });
   return null;
 }
