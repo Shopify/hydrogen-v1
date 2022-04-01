332c5a1ffc392f3f14f9c96d55d4113ceb5330aa
Helen Lin <helen.lin@shopify.com>
2022-04-01 14:21:18 -0700

    fix index test

--- examples/template-hydrogen-default/tests/e2e/index.test.js
+++ examples/template-hydrogen-default/tests/e2e/index.test.js
@@ -20,10 +20,10 @@ describe('index', () => {
 
   it('should have the correct title', async () => {
     await session.visit('/');
-    const heading = await session.page.waitForSelector('h1');
+    const heading = await session.page.locator('h1').first();
     expect(heading).not.toBeNull();
 
-    const text = await session.page.evaluate((h1) => h1.textContent, heading);
+    const text = await heading.textContent();
     expect(text).toBe('Hello, Hydrogen');
   }, 60000);
 });
