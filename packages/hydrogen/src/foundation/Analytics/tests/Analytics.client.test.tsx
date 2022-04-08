554bca8e1739ef611c0a32c138fd30c406e2ba2b
Helen Lin <helen.lin@shopify.com>
2022-04-08 15:42:27 -0700

    add back utm test in a different file

--- packages/hydrogen/src/foundation/Analytics/tests/Analytics.client.test.tsx
+++ packages/hydrogen/src/foundation/Analytics/tests/Analytics.client.test.tsx
@@ -1,42 +1,9 @@
-import React, {useEffect} from 'react';
-import {mount} from '@shopify/react-testing';
-import {Analytics} from '../Analytics.client';
+import React from 'react';
+import {
+  mountComponent,
+  SomeClientComponent,
+} from './analytics-client-test-utils';
 import {ClientAnalytics} from '../ClientAnalytics';
-
-function SomeClientComponent({
-  eventName,
-  reset,
-  callback,
-}: {
-  eventName: string;
-  reset?: Boolean;
-  callback: (payload: any) => void;
-}) {
-  useEffect(() => {
-    const subscriber = ClientAnalytics.subscribe(eventName, (payload: any) => {
-      callback(payload);
-      subscriber.unsubscribe();
-
-      if (reset) {
-        ClientAnalytics.resetPageAnalyticsData();
-      }
-    });
-
-    // Fake publish analytics event because test is creating different
-    // ClientAnalytics instances between <Analytics> and <SomeClientComponet>
-    ClientAnalytics.publish(eventName);
-  });
-  return null;
-}
-
-function mountComponent(analyticsData: any, children: React.ReactChild) {
-  return mount(
-    <>
-      <Analytics analyticsDataFromServer={analyticsData} />
-      {children}
-    </>
-  );
-}
 
 describe('Analytics.client', () => {
   afterEach(async () => {
