285869c9b75b056a2d04cdf69b603623e6d55304
Jason Kurian <JaKXz@users.noreply.github.com>
2022-04-08 13:47:52 -0400

    Update packages/hydrogen/src/components/CartProvider/types.ts
    Co-authored-by: Michelle Vinci <michelle.vinci@shopify.com>

--- packages/hydrogen/src/components/CartProvider/types.ts
+++ packages/hydrogen/src/components/CartProvider/types.ts
@@ -32,7 +32,7 @@ export interface CartProviderProps {
   /** A callback that is invoked when the process to update the cart discount codes begins, but before the discount codes are updated in the Storefront API. */
   onDiscountCodesUpdate?: () => void;
   /**
-   * An object with fields that correspond to the Storefront API's [Cart object](/api/storefront/latest/objects/cart).∏
+   * An object with fields that correspond to the Storefront API's [Cart object](/api/storefront/latest/objects/cart).
    */
   data?: CartFragmentFragment;
 }
