26d6d8807011d2088acb125ef4265b8a2f4fbbaf
Bret Little <bret.little@shopify.com>
2022-04-11 11:38:52 -0400

    Fix tests

--- scripts/polyfillWebRuntime.ts
+++ scripts/polyfillWebRuntime.ts
@@ -1,13 +1,19 @@
-import {fetch} from 'undici';
+import {fetch, Request, Response, Headers} from 'undici';
 import AbortController from 'abort-controller';
+import {TextEncoder, TextDecoder} from 'util';
 
 // Note: globalThis.fetch is already polyfilled by Jest with DOM utilities
 
 // @ts-ignore
-globalThis.Headers = fetch.Headers;
+globalThis.Headers = Headers;
 // @ts-ignore
-globalThis.Request = fetch.Request;
+globalThis.Request = Request;
 // @ts-ignore
-globalThis.Response = fetch.Response;
+globalThis.Response = Response;
 // @ts-ignore
 globalThis.AbortController = AbortController;
+
+// @ts-ignore
+globalThis.TextEncoder = TextEncoder;
+// @ts-ignore
+globalThis.TextDecoder = TextDecoder;
