aa689f81d39ab572f4ba863e1898c5cbd535644e
Fran Dios <fran.dios@shopify.com>
2022-04-12 16:50:58 +0200

    Maybe fix Windows paths

--- scripts/jest-e2e-suite-setup.ts
+++ scripts/jest-e2e-suite-setup.ts
@@ -1,6 +1,6 @@
 import {copy, existsSync} from 'fs-extra';
 import * as http from 'http';
-import {resolve, dirname, basename} from 'path';
+import {resolve, dirname, basename, sep} from 'path';
 import sirv from 'sirv';
 import {
   createServer,
@@ -48,9 +48,11 @@ beforeAll(async () => {
     // start a server in that directory.
 
     const testCustomServe = resolve(
-      testPath
-        .replace(/\.test\.([jt]s)$/, '.serve.$1')
-        .replace(/\/[\w-]+\/tests\//, '/test-utils/')
+      dirname(testPath),
+      '..',
+      '..',
+      'test-utils',
+      basename(testPath).replace(/\.test\.([jt]s)$/, '.serve.$1')
     );
 
     if (existsSync(testCustomServe)) {
