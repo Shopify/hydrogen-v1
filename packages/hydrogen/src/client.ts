4ef92d2d6e32d474beb55755c8b4eca2b58c28b8
Josh Larson <josh.larson@shopify.com>
2022-04-19 09:13:35 -0500

    Add `urlUrl` to client exports

--- packages/hydrogen/src/client.ts
+++ packages/hydrogen/src/client.ts
@@ -2,6 +2,7 @@ export * from './components';
 export * from './hooks';
 export * from './foundation/useServerState';
 export * from './foundation/useShop';
+export * from './foundation/useUrl';
 export * from './foundation/ServerStateProvider';
 export {Head} from './foundation/Head';
 export * from './utilities';
