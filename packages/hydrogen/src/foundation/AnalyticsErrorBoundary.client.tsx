9b2d6e1bf99250fd28a65184e5800f65d740214b
Bret Little <bret.little@shopify.com>
2022-04-19 15:27:18 -0400

    Change to import type

--- packages/hydrogen/src/foundation/AnalyticsErrorBoundary.client.tsx
+++ packages/hydrogen/src/foundation/AnalyticsErrorBoundary.client.tsx
@@ -1,4 +1,5 @@
-import React, {ReactNode} from 'react';
+import React from 'react';
+import type {ReactNode} from 'react';
 import {ErrorBoundary} from 'react-error-boundary';
 
 export default function AnalyticsErrorBoundary({
