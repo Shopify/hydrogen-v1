b497834543e7aef7448f5e21ebabbcb37d178168
Helen Lin <helen.lin@shopify.com>
2022-04-21 15:48:20 -0700

    clean up

--- packages/hydrogen/src/foundation/Analytics/connectors/PerformanceMetrics/PerformanceMetrics.server.tsx
+++ packages/hydrogen/src/foundation/Analytics/connectors/PerformanceMetrics/PerformanceMetrics.server.tsx
@@ -7,33 +7,23 @@ export function request(
   if (url.search === '?performance' && contentType === 'json') {
     const initTime = new Date().getTime();
 
-    console.log('Server performance event');
-    try {
-      fetch('https://monorail-edge.shopifysvc.com/v1/produce', {
-        method: 'post',
-        headers: {
-          'content-type': 'text/plain',
-          'x-forwarded-for': request.headers.get('x-forwarded-for') || '',
-          'user-agent': request.headers.get('user-agent') || '',
+    fetch('https://monorail-edge.shopifysvc.com/v1/produce', {
+      method: 'post',
+      headers: {
+        'content-type': 'text/plain',
+        'x-forwarded-for': request.headers.get('x-forwarded-for') || '',
+        'user-agent': request.headers.get('user-agent') || '',
+      },
+      body: JSON.stringify({
+        schema_id: 'hydrogen_buyer_performance/2.0',
+        payload: data,
+        metadata: {
+          event_created_at_ms: initTime,
+          event_sent_at_ms: new Date().getTime(),
         },
-        body: JSON.stringify({
-          schema_id: 'hydrogen_buyer_performance/2.0',
-          payload: data,
-          metadata: {
-            event_created_at_ms: initTime,
-            event_sent_at_ms: new Date().getTime(),
-          },
-        }),
-      })
-        // .then(response => {
-        //   console.log(response);
-        // })
-        .catch((error) => {
-          console.log(error);
-          // send to bugsnag? oxygen?
-        });
-    } catch (error) {
-      console.log('fetch failed', error);
-    }
+      }),
+    }).catch((error) => {
+      // send to bugsnag? oxygen?
+    });
   }
 }
