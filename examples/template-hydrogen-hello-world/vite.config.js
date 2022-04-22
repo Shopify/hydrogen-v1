11041d80bdaf7a14bb922edf53f433a1868889ad
Matt Seccafien <matt.seccafien@shopify.com>
2022-04-22 14:07:41 +0200

    Update vite.config.js

--- examples/template-hydrogen-hello-world/vite.config.js
+++ examples/template-hydrogen-hello-world/vite.config.js
@@ -3,7 +3,6 @@ import hydrogen from '@shopify/hydrogen/plugin';
 
 import shopifyConfig from './shopify.config';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [hydrogen(shopifyConfig)],
 });
