2db87096e0cdfb097d6a70f9ecde81599291a59c
Josh Larson <josh.larson@shopify.com>
2022-04-29 10:50:44 -0400

    I don't think we need this?

--- shopify.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  defaultLocale: 'en',
-  storeDomain: 'hydrogen-preview.myshopify.com',
-  storefrontToken: '46ab997137fbd94d3701f7a38c5bb1ba',
-  storefrontApiVersion: 'unstable',
-};
