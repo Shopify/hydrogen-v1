398aa364b387ab1fc8ddbb8cbf41ba57c9ffd703
Josh Larson <josh.larson@shopify.com>
2022-04-29 11:58:24 -0400

    Add shopify config file to root for use by Oxygen

---
+++ shopify.config.js
@@ -0,0 +1,8 @@
+// This is only here to trick Oxygen into thinking this is a valid deployment.
+
+export default {
+  defaultLocale: 'en',
+  storeDomain: 'hydrogen-preview.myshopify.com',
+  storefrontToken: '46ab997137fbd94d3701f7a38c5bb1ba',
+  storefrontApiVersion: 'unstable',
+};
