cbf35b31139da63f90bbf5c9565c5febb8228e7a
Helen Lin <helen.lin@shopify.com>
2022-05-06 16:12:40 -0700

    fix streaming on CF

--- packages/hydrogen/src/streaming.server.ts
+++ packages/hydrogen/src/streaming.server.ts
@@ -42,7 +42,11 @@ export const ssrRenderToReadableStream = _ssrRenderToReadableStream as (
 ) => Promise<ReadableStream<Uint8Array> & {allReady: Promise<void>}>;
 
 export async function isStreamingSupported() {
-  return Boolean(globalThis.Oxygen?.env?.HYDROGEN_ENABLE_WORKER_STREAMING);
+  // rename this function to Oxygen specific check
+  if (globalThis.Oxygen) {
+    return Boolean(globalThis.Oxygen?.env?.HYDROGEN_ENABLE_WORKER_STREAMING);
+  }
+  return true;
 }
 
 export async function bufferReadableStream(
