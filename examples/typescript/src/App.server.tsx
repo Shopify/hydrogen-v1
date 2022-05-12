976724dd3dfe3af17da009055db0aac13214aa95
Matt Seccafien <matt.seccafien@shopify.com>
2022-05-12 06:40:52 +0200

    move to examples

--- examples/typescript/src/App.server.tsx
+++ examples/typescript/src/App.server.tsx
@@ -3,12 +3,12 @@ import renderHydrogen from '@shopify/hydrogen/entry-server';
 import {Router, FileRoutes, ShopifyProvider} from '@shopify/hydrogen';
 import {Suspense} from 'react';
 
-function App() {
+function App({routes}: any) {
   return (
     <Suspense fallback={null}>
       <ShopifyProvider>
         <Router>
-          <FileRoutes />
+          <FileRoutes routes={routes} />
         </Router>
       </ShopifyProvider>
     </Suspense>
