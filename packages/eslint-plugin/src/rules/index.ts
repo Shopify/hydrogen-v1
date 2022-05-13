5d18b9ccf4c79809ce9bb8cda0403769c0b56f4e
Matt Seccafien <matt.seccafien@shopify.com>
2022-05-13 12:53:45 +0200

    Update index.ts

--- packages/eslint-plugin/src/rules/index.ts
+++ packages/eslint-plugin/src/rules/index.ts
@@ -1,9 +1,11 @@
 import {clientComponentBannedHooks} from './client-component-banned-hooks';
 import {serverComponentBannedHooks} from './server-component-banned-hooks';
 import {preferImageComponent} from './prefer-image-component';
+import {serverNoJsonParse} from './server-no-json-parse';
 
 export const rules: {[key: string]: any} = {
   'client-component-banned-hooks': clientComponentBannedHooks,
   'server-component-banned-hooks': serverComponentBannedHooks,
   'prefer-image-component': preferImageComponent,
+  'server-no-json-parse': serverNoJsonParse,
 };
