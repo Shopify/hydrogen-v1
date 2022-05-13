8014bbfca603a79f953a3efe65bd7d21e9f16154
Bret Little <bret.little@shopify.com>
2022-05-13 16:34:48 -0400

    Fix recovery page

--- templates/template-hydrogen-default/src/components/AccountRecoverForm.client.jsx
+++ templates/template-hydrogen-default/src/components/AccountRecoverForm.client.jsx
@@ -8,90 +8,97 @@ export default function AccountRecoverForm() {
   const [emailError, setEmailError] = React.useState(null);
 
   function emailValidation(email) {
-    if (!email || email.trim() === '') {
-      return 'Please enter an email';
-    }
+    if (email.validity.valid) return null;
+
+    return email.validity.valueMissing
+      ? 'Please enter an email'
+      : 'Please enter a valid email';
   }
 
-  async function onSubmit() {
+  async function onSubmit(event) {
+    event.preventDefault();
+
     setEmailError(null);
     setSubmitError(null);
 
-    const newEmailError = emailValidation(email);
+    const newEmailError = emailValidation(event.target.email);
+
     if (newEmailError) {
       setEmailError(newEmailError);
       return;
     }
 
-    const response = await callAccountRecoverApi({
+    await callAccountRecoverApi({
       email,
     });
 
-    if (response?.error?.length > 0) {
-      setSubmitError(response.error[0].message);
-      return;
-    }
-
+    setEmail('');
     setSubmitSuccess(true);
   }
 
   return (
-    <>
-      {submitSuccess ? (
-        <>
-          <h1 className="text-2xl font-bold">Request Sent.</h1>
-          <p className="mt-6">
-            If that email address is in our system, you will receive an email
-            with instructions about how to reset your password in a few minutes.
-          </p>
-        </>
-      ) : (
-        <>
-          <h1 className="text-2xl font-bold">Forgot Password.</h1>
-          <p className="mt-6">
-            Enter the email address associated with your account to recieve a
-            link to reset your password.
-          </p>
-        </>
-      )}
-      <form
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mt-6 mb-4"
-        onSubmit={onSubmit}
-      >
-        {submitError && (
-          <div className="flex items-center justify-center mb-6 bg-zinc-500">
-            <p className="m-4 text-s text-white">{submitError}</p>
-          </div>
+    <div className="flex justify-center">
+      <div className="max-w-md w-full">
+        {submitSuccess ? (
+          <>
+            <h1 className="text-4xl">Request Sent.</h1>
+            <p className="mt-4">
+              If that email address is in our system, you will receive an email
+              with instructions about how to reset your password in a few
+              minutes.
+            </p>
+          </>
+        ) : (
+          <>
+            <h1 className="text-4xl">Forgot Password.</h1>
+            <p className="mt-4">
+              Enter the email address associated with your account to recieve a
+              link to reset your password.
+            </p>
+          </>
         )}
-        <div className="mb-6">
-          <input
-            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline${
-              emailError ? ' border-red-500 mb-3' : ''
-            }`}
-            id="email"
-            name="email"
-            type="email"
-            autoComplete="email"
-            placeholder="Email address"
-            aria-label="Email address"
-            value={email}
-            onChange={(event) => {
-              setEmail(event.target.value);
-            }}
-          />
-          {emailError && <p className="text-red-500 text-xs">{emailError}</p>}
-        </div>
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold uppercase py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
-            onClick={onSubmit}
-          >
-            Request Reset Link
-          </button>
-        </div>
-      </form>
-    </>
+        <form noValidate className="pt-6 pb-8 mt-4 mb-4" onSubmit={onSubmit}>
+          {submitError && (
+            <div className="flex items-center justify-center mb-6 bg-zinc-500">
+              <p className="m-4 text-s text-white">{submitError}</p>
+            </div>
+          )}
+          <div className="mb-4">
+            <input
+              className={`mb-1 appearance-none border w-full py-2 px-3 text-gray-800 placeholder:text-gray-500 leading-tight focus:outline-none focus:shadow-outline ${
+                emailError ? ' border-red-500' : 'border-gray-900'
+              }`}
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              placeholder="Email address"
+              aria-label="Email address"
+              value={email}
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+            />
+            <p
+              className={`text-red-500 text-xs ${
+                !emailError ? 'invisible' : ''
+              }`}
+            >
+              {emailError} &nbsp;
+            </p>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-gray-900 text-white uppercase py-2 px-4 focus:outline-none focus:shadow-outline block w-full"
+              type="submit"
+            >
+              Request Reset Link
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
