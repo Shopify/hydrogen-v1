1a09d086841829c8092e43c513a03f2cc7afcdd9
Helen Lin <helen.lin@shopify.com>
2022-05-20 16:08:22 -0700

    fix server side analytics

--- packages/hydrogen/src/foundation/Analytics/connectors/PerformanceMetrics/PerformanceMetrics.server.tsx
+++ packages/hydrogen/src/foundation/Analytics/connectors/PerformanceMetrics/PerformanceMetrics.server.tsx
@@ -1,9 +1,10 @@
 export function request(
-  request: Request,
+  requestUrl: string,
+  requestHeader: Headers,
   data?: any,
   contentType?: string
 ): void {
-  const url = new URL(request.url);
+  const url = new URL(requestUrl);
   if (url.search === '?performance' && contentType === 'json') {
     const initTime = new Date().getTime();
 
@@ -11,8 +12,8 @@ export function request(
       method: 'post',
       headers: {
         'content-type': 'text/plain',
-        'x-forwarded-for': request.headers.get('x-forwarded-for') || '',
-        'user-agent': request.headers.get('user-agent') || '',
+        'x-forwarded-for': requestHeader.get('x-forwarded-for') || '',
+        'user-agent': requestHeader.get('user-agent') || '',
       },
       body: JSON.stringify({
         schema_id: 'hydrogen_buyer_performance/2.0',
--- packages/hydrogen/src/foundation/Analytics/connectors/Shopify/ShopifyAnalyticsServerConnector.server.tsx
+++ packages/hydrogen/src/foundation/Analytics/connectors/Shopify/ShopifyAnalyticsServerConnector.server.tsx
@@ -1,13 +1,14 @@
 export function request(
-  request: Request,
+  requestUrl: string,
+  requestHeader: Headers,
   data?: any,
   contentType?: string
 ): void {
-  const url = new URL(request.url);
+  const url = new URL(requestUrl);
   if (url.search === '?shopify' && contentType === 'json') {
     data.events.forEach((event: any) => {
-      event.payload.client_ip_address = request.headers.get('x-forwarded-for');
-      event.payload.client_user_agent = request.headers.get('user-agent');
+      event.payload.client_ip_address = requestHeader.get('x-forwarded-for');
+      event.payload.client_user_agent = requestHeader.get('user-agent');
     });
 
     fetch('https://monorail-edge.shopifysvc.com/unstable/produce_batch', {
