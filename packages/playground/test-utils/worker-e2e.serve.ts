fa54a20dd9ced16fbbb2efd384a2d09447bd7768
Matt Seccafien <matt.seccafien@shopify.com>
2022-05-26 00:13:27 +0200

    Update CLI dependencies and commands (#1338)
    * update cli dependencies and commands
    
    * Update to 2.0.10
    
    * Fix oxygen deployment
    
    * update playground
    
    * Use node build for vitest CI
    
    * Fix e2e tests
    
    * Refactor initial scripts
    
    * Upgrade Vitest
    
    * Update deployment docs for new build commands
    
    * Remove node stuff from css-modules example
    
    * Add note and instructions and Node peer dependencies
    
    * Simplify linter commands
    
    * Apply suggestions from code review
    
    Co-authored-by: Michelle Vinci <michelle.vinci@shopify.com>
    
    Co-authored-by: Josh Larson <josh.larson@shopify.com>
    Co-authored-by: Michelle Vinci <michelle.vinci@shopify.com>

--- packages/playground/test-utils/worker-e2e.serve.ts
+++ packages/playground/test-utils/worker-e2e.serve.ts
@@ -9,10 +9,7 @@ export const port = 9528;
 export async function serve(root: string, isProd: boolean) {
   // we build first, regardless of whether it's prod/build mode
   // because Vite doesn't support the concept of a "webworker server"
-  execSync(
-    'yarn build:client --logLevel silent && yarn build:worker --logLevel silent',
-    {cwd: root}
-  );
+  execSync('yarn build', {cwd: root});
 
   // @ts-ignore
   const {app} = await createServer({root, isProd});
