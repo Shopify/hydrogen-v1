a61d87cae8202dc417dc52fc1d7e7236a6929a68
Helen Lin <helen.lin@shopify.com>
2022-05-30 15:49:27 -0700

    prettier

--- templates/demo-store/tests/e2e/shopify-analytics.test.js
+++ templates/demo-store/tests/e2e/shopify-analytics.test.js
@@ -20,48 +20,56 @@ describe('analytics', () => {
     await hydrogen.cleanUp();
   });
 
-  it('should emit page-view event', async () => {
-    const [request] = await Promise.all([
-      session.page.waitForRequest(eventsEndpoint),
-      session.visit('/'),
-    ]);
+  it(
+    'should emit page-view event',
+    async () => {
+      const [request] = await Promise.all([
+        session.page.waitForRequest(eventsEndpoint),
+        session.visit('/'),
+      ]);
 
-    const shopifyEvents = request.postDataJSON();
-    expect(request.url()).toEqual(eventsEndpoint);
+      const shopifyEvents = request.postDataJSON();
+      expect(request.url()).toEqual(eventsEndpoint);
 
-    const event = shopifyEvents.events[0];
-    const payload = event.payload;
+      const event = shopifyEvents.events[0];
+      const payload = event.payload;
 
-    expect(event.schema_id).toContain('trekkie_storefront_page_view');
-    expect(payload.shopId).not.toEqual(0);
-    expect(payload.pageType).toEqual('index');
-    expect(payload.isMerchantRequest).toEqual(true);
-  }, DEFAULT_DELAY);
+      expect(event.schema_id).toContain('trekkie_storefront_page_view');
+      expect(payload.shopId).not.toEqual(0);
+      expect(payload.pageType).toEqual('index');
+      expect(payload.isMerchantRequest).toEqual(true);
+    },
+    DEFAULT_DELAY,
+  );
 
-  it('should emit page-view on sub load', async () => {
-    const collectionPath = '/collections/freestyle-collection';
-    // Full load
-    await Promise.all([
-      session.page.waitForRequest(eventsEndpoint),
-      session.visit('/'),
-    ]);
+  it(
+    'should emit page-view on sub load',
+    async () => {
+      const collectionPath = '/collections/freestyle-collection';
+      // Full load
+      await Promise.all([
+        session.page.waitForRequest(eventsEndpoint),
+        session.visit('/'),
+      ]);
 
-    // Sub load
-    const [request] = await Promise.all([
-      session.page.waitForRequest(eventsEndpoint),
-      session.page.click(`a[href="${collectionPath}"]`),
-    ]);
+      // Sub load
+      const [request] = await Promise.all([
+        session.page.waitForRequest(eventsEndpoint),
+        session.page.click(`a[href="${collectionPath}"]`),
+      ]);
 
-    const shopifyEvents = request.postDataJSON();
-    expect(request.url()).toEqual(eventsEndpoint);
+      const shopifyEvents = request.postDataJSON();
+      expect(request.url()).toEqual(eventsEndpoint);
 
-    const event = shopifyEvents.events[0];
-    const payload = event.payload;
+      const event = shopifyEvents.events[0];
+      const payload = event.payload;
 
-    expect(event.schema_id).toContain('trekkie_storefront_page_view');
-    expect(payload.shopId).not.toEqual(0);
-    expect(payload.pageType).toEqual('collection');
-    expect(payload.resourceType).toEqual('collection');
-    expect(payload.isMerchantRequest).toEqual(true);
-  }, DEFAULT_DELAY);
+      expect(event.schema_id).toContain('trekkie_storefront_page_view');
+      expect(payload.shopId).not.toEqual(0);
+      expect(payload.pageType).toEqual('collection');
+      expect(payload.resourceType).toEqual('collection');
+      expect(payload.isMerchantRequest).toEqual(true);
+    },
+    DEFAULT_DELAY,
+  );
 });
