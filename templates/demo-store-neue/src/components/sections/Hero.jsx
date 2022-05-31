bd0d775d9d447d1013924883fafb7adc60c40c45
Benjamin Sehl <ben.sehl@shopify.com>
2022-05-31 16:36:20 -0400

    Completes desktop hero with dynamic data

--- templates/demo-store-neue/src/components/sections/Hero.jsx
+++ templates/demo-store-neue/src/components/sections/Hero.jsx
@@ -1,4 +1,4 @@
-import {Image, Link} from '@shopify/hydrogen';
+import {Image, Video, Link} from '@shopify/hydrogen';
 import {Heading, Text} from '../elements';
 import {hero as mockData} from '~/lib/placeholders';
 
@@ -11,19 +11,11 @@ export default function Hero({data = mockData, height, top}) {
         className={`relative justify-end flex flex-col w-full ${top &&
           '-mt-nav'} ${height === 'full' ? 'h-screen' : 'h-[50rem]'}`}
       >
-        <div className="absolute inset-0 grid flex-grow grid-flow-col pointer-events-none -z-10 content-stretch overflow-clip">
-          {/* {images.map((image, i) => (
-            <Image
-              key={i}
-              width={1480}
-              height={1480}
-              alt={`Image of ${title}`}
-              className="block object-cover w-auto h-full"
-              src={image}
-            />
-          ))} */}
-          {spread.value && <></>}
-          {spread_secondary.value && <></>}
+        <div className="absolute inset-0 grid flex-grow grid-flow-col pointer-events-none auto-cols-fr -z-10 content-stretch overflow-clip">
+          {spread?.reference && <SpreadMedia data={spread.reference} />}
+          {spread_secondary?.reference && (
+            <SpreadMedia data={spread_secondary.reference} />
+          )}
         </div>
         <div className="flex flex-col items-baseline justify-between gap-4 px-12 py-8 bg-gradient-to-t from-primary/60 text-contrast">
           <Heading as="h2" size="display" className="max-w-prose-narrow">
@@ -41,4 +33,36 @@ export default function Hero({data = mockData, height, top}) {
       </section>
     </Link>
   );
+}
+
+function SpreadMedia({data}) {
+  if (data.mediaContentType === 'VIDEO') {
+    return (
+      <Video
+        width={1200}
+        height={1600}
+        alt={data.alt || 'Marketing Banner Video'}
+        className="block object-cover w-full h-full"
+        data={data}
+        muted
+        loop
+        playsInline
+        autoPlay
+      />
+    );
+  }
+
+  if (data.mediaContentType === 'IMAGE') {
+    return (
+      <Image
+        width={1200}
+        height={1600}
+        alt={data.alt || 'Marketing Banner Image'}
+        className="block object-cover w-full h-full"
+        data={data.image}
+      />
+    );
+  }
+
+  return null;
 }
