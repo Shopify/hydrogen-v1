3546e3c392a194dfc25587ebd09b711dafa5c7e3
Fran Dios <fran.dios@shopify.com>
2022-06-03 18:53:52 +0900

    Fix CJS interop and Jest

--- packages/hydrogen/src/framework/plugin.ts
+++ packages/hydrogen/src/framework/plugin.ts
@@ -15,7 +15,7 @@ import cssModulesRsc from './plugins/vite-plugin-css-modules-rsc';
 import clientImports from './plugins/vite-plugin-client-imports';
 import suppressWarnings from './plugins/vite-plugin-hydrogen-suppress-warnings';
 
-export default (pluginOptions: HydrogenVitePluginOptions = {}) => {
+const hydrogenPlugin = (pluginOptions: HydrogenVitePluginOptions = {}) => {
   return [
     process.env.VITE_INSPECT && inspect(),
     hydrogenConfig(),
@@ -33,3 +33,7 @@ export default (pluginOptions: HydrogenVitePluginOptions = {}) => {
     pluginOptions.purgeQueryCacheOnBuild && purgeQueryCache(),
   ] as Plugin[];
 };
+
+// @ts-ignore
+export = hydrogenPlugin; // TS syntax to support CJS interop
+export default hydrogenPlugin; // For ESM
