88f962e814e63477ba6fb2af4b767d4a808d4866
Scott Dixon <scott.dixon@shopify.com>
2022-06-06 11:20:32 +1000

    Demo store neue: article - formate date and cache

--- templates/demo-store/src/routes/journal/[handle].server.jsx
+++ templates/demo-store/src/routes/journal/[handle].server.jsx
@@ -1,9 +1,22 @@
-import {useShop, useShopQuery, Seo, gql, Image} from '@shopify/hydrogen';
+import {
+  useShop,
+  useShopQuery,
+  Seo,
+  gql,
+  Image,
+  CacheDays,
+} from '@shopify/hydrogen';
 
 import {DefaultLayout as Layout} from '~/components/layouts';
+const dateFormatOptions = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+};
 
-export default function Page({params}) {
-  const {languageCode} = useShop();
+export default function Page({params, response}) {
+  response.cache(CacheDays());
+  const {languageCode, locale} = useShop();
 
   const {handle} = params;
   const {data} = useShopQuery({
@@ -11,19 +24,23 @@ export default function Page({params}) {
     variables: {language: languageCode, handle},
   });
 
-  const article = data.blog.articleByHandle;
-
+  const {title, publishedAt, contentHtml, author} = data.blog.articleByHandle;
+  const formattedDate = new Intl.DateTimeFormat(
+    locale,
+    dateFormatOptions,
+  ).format(new Date(publishedAt));
   return (
     <Layout>
       <Seo type="page" data={data.blog.articleByHandle} />
       <section className="w-2/4 m-auto gap-4 md:gap-8 p-4 py-6 md:p-8 lg:p-12">
-        <h1 className="text-4xl font-bold w-4/5 m-auto">{article.title}</h1>
+        <h1 className="text-4xl font-bold w-4/5 m-auto">{title}</h1>
         <span className="block m-auto w-4/5 mt-6 text-base">
-          June 22, 2022 · Michelle Vinci
+          {formattedDate} &middot; {author.name}
         </span>
+        <span></span>
         <Image data={data.blog.articleByHandle.image} className="mt-16" />
         <div
-          dangerouslySetInnerHTML={{__html: article.contentHtml}}
+          dangerouslySetInnerHTML={{__html: contentHtml}}
           className="prose w-4/5 m-auto mt-16 max-w-max mb-24"
         />
       </section>
@@ -38,6 +55,10 @@ const QUERY = gql`
       articleByHandle(handle: $handle) {
         title
         contentHtml
+        publishedAt
+        author {
+          name
+        }
         image {
           id
           altText
