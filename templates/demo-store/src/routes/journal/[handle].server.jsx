cd6e24b03765b1bfe946b52c661fde43bdf902ba
Scott Dixon <scott.dixon@shopify.com>
2022-06-07 09:04:00 +1000

    Demo store neue: article - font todo

--- templates/demo-store/src/routes/journal/[handle].server.jsx
+++ templates/demo-store/src/routes/journal/[handle].server.jsx
@@ -36,6 +36,7 @@ export default function Page({params, response}) {
   return (
     <Layout>
       <Head>
+        {/* TODO: show custom font implementation, don't use Google fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
