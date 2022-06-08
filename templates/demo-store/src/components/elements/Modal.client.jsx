030d8d875c848a5e619f803cf67a81d27b5a774c
davecyen@gmail.com <davecyen@gmail.com>
2022-06-08 09:24:31 -0700

    Refactored modal

---
+++ templates/demo-store/src/components/elements/Modal.client.jsx
@@ -0,0 +1,24 @@
+export default function Modal({setShowModal, children}) {
+  return (
+    <div
+      className="relative z-10"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+      id="modal-bg"
+      onClick={() => setShowModal(false)}
+    >
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <div className="fixed z-10 inset-0 overflow-y-auto">
+        <div className="flex items-center justify-center min-h-full p-4 text-center sm:p-0">
+          <div
+            className="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6"
+            onClick={(e) => e.stopPropagation()}
+          >
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
