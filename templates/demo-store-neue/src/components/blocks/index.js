12088570ba9d06e602f8ccf7ef5fec0f1cf0a6cf
Juan Pablo Prieto <juanpablo.prieto@shopify.com>
2022-06-09 11:29:19 -0700

    remove unused MoneyPrice

--- templates/demo-store-neue/src/components/blocks/index.js
+++ templates/demo-store-neue/src/components/blocks/index.js
@@ -1,5 +1,4 @@
 export {CountrySelector} from './CountrySelector.client';
 export {LocationCard} from './LocationCard';
-export {MoneyPrice} from './MoneyPrice.client';
 export {OrderCard} from './OrderCard.client';
 export {ProductCard} from './ProductCard.client';
