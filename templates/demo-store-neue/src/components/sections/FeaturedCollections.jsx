539cf7423a4fb1d645db31230be604417864b3bf
Benjamin Sehl <ben.sehl@shopify.com>
2022-06-10 11:40:53 -0400

    Fixes layout for Featured Collections component to exclude collections with missing images

--- templates/demo-store-neue/src/components/sections/FeaturedCollections.jsx
+++ templates/demo-store-neue/src/components/sections/FeaturedCollections.jsx
@@ -4,25 +4,32 @@ import {Heading, Section, Grid} from '~/components/elements';
 
 // TODO: This should be consolidated with Locations into a more generic presentational component
 export function FeaturedCollections({data, title = 'Collections', ...props}) {
+  const items = data.filter((item) => item.image).length;
+
   return (
     <Section heading={title} {...props}>
-      <Grid items={data.length}>
-        {data.map((collection) => (
-          <Link key={collection.id} to={`/collections/${collection.handle}`}>
-            <div className="grid gap-4">
-              {collection?.image && (
-                <Image
-                  className="rounded shadow-border overflow-clip inline-block aspect-[5/4] md:aspect-[3/2] object-cover"
-                  width={'100%'}
-                  height={336}
-                  alt={`Image of ${collection.title}`}
-                  data={collection.image}
-                />
-              )}
-              <Heading size="copy">{collection.title}</Heading>
-            </div>
-          </Link>
-        ))}
+      <Grid items={items}>
+        {data.map((collection) => {
+          if (!collection?.image) {
+            return null;
+          }
+          return (
+            <Link key={collection.id} to={`/collections/${collection.handle}`}>
+              <div className="grid gap-4">
+                {collection?.image && (
+                  <Image
+                    className="rounded shadow-border overflow-clip inline-block aspect-[5/4] md:aspect-[3/2] object-cover"
+                    width={'100%'}
+                    height={336}
+                    alt={`Image of ${collection.title}`}
+                    data={collection.image}
+                  />
+                )}
+                <Heading size="copy">{collection.title}</Heading>
+              </div>
+            </Link>
+          );
+        })}
       </Grid>
     </Section>
   );
