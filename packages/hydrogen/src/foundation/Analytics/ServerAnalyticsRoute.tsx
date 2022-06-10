nolotus/hydrogen

Add a built-in health check endpoint

packages/hydrogen/src/foundation/Analytics/ServerAnalyticsRoute.tsx
@@ -1,10 +1,11 @@
-import type {ServerAnalyticsConnector} from '../../types';
+import type {HydrogenConfig} from '../../types';
 import {log} from '../../utilities/log';
 
-export function ServerAnalyticsRoute(
+export async function ServerAnalyticsRoute(
   request: Request,
-  serverAnalyticsConnectors?: Array<ServerAnalyticsConnector>
+  {hydrogenConfig}: {hydrogenConfig: HydrogenConfig}
 ) {
+  const serverAnalyticsConnectors = hydrogenConfig.serverAnalyticsConnectors;
   if (request.headers.get('Content-Length') === '0') {
     serverAnalyticsConnectors?.forEach((connector) => {
       connector.request(request);

packages/hydrogen/src/utilities/apiRoutes.ts
@@ -23,8 +23,9 @@ type RequestOptions = {
   params: RouteParams;
   queryShop: (args: QueryShopArgs) => Promise<any>;
   session: SessionApi | null;
+  hydrogenConfig: HydrogenConfig;
 };
-type ResourceGetter = (
+export type ResourceGetter = (
   request: Request,
   requestOptions: RequestOptions
 ) => Promise<Response | Object | String>;
@@ -195,8 +196,14 @@ function queryShopBuilder(
 export async function renderApiRoute(
   request: ServerComponentRequest,
   route: ApiRouteMatch,
-  shopifyConfig: HydrogenConfig['shopify'],
-  session?: SessionStorageAdapter
+  hydrogenConfig: HydrogenConfig,
+  {
+    session,
+    suppressLog,
+  }: {
+    session?: SessionStorageAdapter;
+    suppressLog?: boolean;
+  }
 ): Promise<Response | Request> {
   let response;
   const log = getLoggerWithContext(request);
@@ -205,7 +212,8 @@ export async function renderApiRoute(
   try {
     response = await route.resource(request, {
       params: route.params,
-      queryShop: queryShopBuilder(shopifyConfig, request),
+      queryShop: queryShopBuilder(hydrogenConfig.shopify, request),
+      hydrogenConfig,
       session: session
         ? {
             async get() {
@@ -247,11 +255,13 @@ export async function renderApiRoute(
     response = new Response('Error processing: ' + request.url, {status: 500});
   }
 
-  logServerResponse(
-    'api',
-    request as ServerComponentRequest,
-    (response as Response).status ?? 200
-  );
+  if (!suppressLog) {
+    logServerResponse(
+      'api',
+      request as ServerComponentRequest,
+      (response as Response).status ?? 200
+    );
+  }
 
   return response;
 }

Shopify/hydrogen-v1

packages/hydrogen/src/foundation/Analytics/ServerAnalyticsRoute.tsx
@@ -1,12 +1,13 @@
-import type {ServerAnalyticsConnector} from '../../types';
+import type {ResolvedHydrogenConfig} from '../../types';
 import {log} from '../../utilities/log';
 
-export function ServerAnalyticsRoute(
+export async function ServerAnalyticsRoute(
   request: Request,
-  serverAnalyticsConnectors?: Array<ServerAnalyticsConnector>
+  {hydrogenConfig}: {hydrogenConfig: ResolvedHydrogenConfig}
 ) {
   const requestHeader = request.headers;
   const requestUrl = request.url;
+  const serverAnalyticsConnectors = hydrogenConfig.serverAnalyticsConnectors;
 
   if (requestHeader.get('Content-Length') === '0') {
     serverAnalyticsConnectors?.forEach((connector) => {

packages/hydrogen/src/utilities/apiRoutes.ts
@@ -1,5 +1,5 @@
 import {
-  InlineHydrogenConfig,
+  ResolvedHydrogenConfig,
   ResolvedHydrogenRoutes,
   ImportGlobEagerOutput,
 } from '../types';
@@ -22,8 +22,9 @@ type RequestOptions = {
   params: RouteParams;
   queryShop: (args: QueryShopArgs) => Promise<any>;
   session: SessionApi | null;
+  hydrogenConfig: ResolvedHydrogenConfig;
 };
-type ResourceGetter = (
+export type ResourceGetter = (
   request: Request,
   requestOptions: RequestOptions
 ) => Promise<Response | Object | String>;
@@ -146,7 +147,7 @@ interface QueryShopArgs {
 }
 
 function queryShopBuilder(
-  shopifyConfigGetter: InlineHydrogenConfig['shopify'],
+  shopifyConfigGetter: ResolvedHydrogenConfig['shopify'],
   request: HydrogenRequest
 ) {
   return async function queryShop<T>({
@@ -191,8 +192,14 @@ function queryShopBuilder(
 export async function renderApiRoute(
   request: HydrogenRequest,
   route: ApiRouteMatch,
-  shopifyConfig: InlineHydrogenConfig['shopify'],
-  session?: SessionStorageAdapter
+  hydrogenConfig: ResolvedHydrogenConfig,
+  {
+    session,
+    suppressLog,
+  }: {
+    session?: SessionStorageAdapter;
+    suppressLog?: boolean;
+  }
 ): Promise<Response | Request> {
   let response;
   const log = getLoggerWithContext(request);
@@ -201,7 +208,8 @@ export async function renderApiRoute(
   try {
     response = await route.resource(request, {
       params: route.params,
-      queryShop: queryShopBuilder(shopifyConfig, request),
+      queryShop: queryShopBuilder(hydrogenConfig.shopify, request),
+      hydrogenConfig,
       session: session
         ? {
             async get() {
@@ -243,11 +251,13 @@ export async function renderApiRoute(
     response = new Response('Error processing: ' + request.url, {status: 500});
   }
 
-  logServerResponse(
-    'api',
-    request as HydrogenRequest,
-    (response as Response).status ?? 200
-  );
+  if (!suppressLog) {
+    logServerResponse(
+      'api',
+      request as HydrogenRequest,
+      (response as Response).status ?? 200
+    );
+  }
 
   return response;
 }