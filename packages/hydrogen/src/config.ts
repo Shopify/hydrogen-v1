c61f0cc0fc5e84ed1b6c47fc5c653e984121138a
Fran Dios <fran.dios@shopify.com>
2022-06-10 18:40:58 +0900

    Move config utilities to the config entry point

--- packages/hydrogen/src/config.ts
+++ packages/hydrogen/src/config.ts
@@ -3,3 +3,9 @@ import type {InlineHydrogenConfig} from './types';
 export const defineConfig = (params: InlineHydrogenConfig) => params;
 
 export type {InlineHydrogenConfig as HydrogenConfig};
+
+export * as ShopifyServerAnalyticsConnector from './foundation/Analytics/connectors/Shopify/ServerAnalyticsConnector.server';
+export * as PerformanceMetricsServerAnalyticsConnector from './foundation/Analytics/connectors/PerformanceMetrics/ServerAnalyticsConnector.server';
+
+export {CookieSessionStorage} from './foundation/CookieSessionStorage/CookieSessionStorage';
+export {MemorySessionStorage} from './foundation/MemorySessionStorage/MemorySessionStorage';
