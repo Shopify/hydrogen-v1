8b6e31e45b379b822e5be47733ec0d223995c3e1
Juan Pablo Prieto <juanpablo.prieto@shopify.com>
2022-06-10 16:59:39 -0700

    migrate account blocks

--- templates/demo-store-neue/src/components/blocks/index.js
+++ templates/demo-store-neue/src/components/blocks/index.js
@@ -1,4 +1,5 @@
 export {CountrySelector} from './CountrySelector.client';
+export {Drawer, useDrawer} from './Drawer.client';
+export {Modal} from './Modal.client';
 export {OrderCard} from './OrderCard.client';
 export {ProductCard} from './ProductCard.client';
-export {Drawer} from './Drawer.client';
