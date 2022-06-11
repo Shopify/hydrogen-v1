8f578751a7ca827162153a2e28e49fd9c070db15
Juan Pablo Prieto <juanpablo.prieto@shopify.com>
2022-06-10 17:00:44 -0700

    migrate account utils

--- templates/demo-store-neue/src/lib/utils.js
+++ templates/demo-store-neue/src/lib/utils.js
@@ -179,3 +179,56 @@ export function getApiErrorMessage(field, data, errors) {
     return data[field].customerUserErrors[0].message;
   return null;
 }
+
+// TODO: rename to translateStatus
+export function statusMessage(status) {
+  const translations = {
+    ATTEMPTED_DELIVERY: 'Attempted delivery',
+    CANCELED: 'Canceled',
+    CONFIRMED: 'Confirmed',
+    DELIVERED: 'Delivered',
+    FAILURE: 'Failure',
+    FULFILLED: 'Fulfilled',
+    IN_PROGRESS: 'In Progress',
+    IN_TRANSIT: 'In transit',
+    LABEL_PRINTED: 'Label printed',
+    LABEL_PURCHASED: 'Label purchased',
+    LABEL_VOIDED: 'Label voided',
+    MARKED_AS_FULFILLED: 'Marked as fulfilled',
+    NOT_DELIVERED: 'Not delivered',
+    ON_HOLD: 'On Hold',
+    OPEN: 'Open',
+    OUT_FOR_DELIVERY: 'Out for delivery',
+    PARTIALLY_FULFILLED: 'Partially Fulfilled',
+    PENDING_FULFILLMENT: 'Pending',
+    PICKED_UP: 'Displayed as Picked up',
+    READY_FOR_PICKUP: 'Ready for pickup',
+    RESTOCKED: 'Restocked',
+    SCHEDULED: 'Scheduled',
+    SUBMITTED: 'Submitted',
+    UNFULFILLED: 'Unfulfilled',
+  };
+  try {
+    return translations?.[status];
+  } catch (error) {
+    return status;
+  }
+}
+
+export function emailValidation(email) {
+  if (email.validity.valid) return null;
+
+  return email.validity.valueMissing
+    ? 'Please enter an email'
+    : 'Please enter a valid email';
+}
+
+export function passwordValidation(password) {
+  if (password.validity.valid) return null;
+
+  if (password.validity.valueMissing) {
+    return 'Please enter a password';
+  }
+
+  return 'Password must be at least 6 characters';
+}
