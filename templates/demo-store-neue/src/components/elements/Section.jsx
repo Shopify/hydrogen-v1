4a9b3725b3d7f23327bb225494abd17c5a2902fe
Benjamin Sehl <ben.sehl@shopify.com>
2022-06-12 15:43:49 -0400

    Updates Section spacing and Grid layout

--- templates/demo-store-neue/src/components/elements/Section.jsx
+++ templates/demo-store-neue/src/components/elements/Section.jsx
@@ -14,16 +14,16 @@ export function Section({
   ...props
 }) {
   const paddings = {
-    x: 'px-4 md:px-8 lg:px-12',
+    x: 'px-6 md:px-8 lg:px-12',
     y: 'py-6 md:py-8 lg:py-12',
     swimlane: 'pt-4 md:pt-8 lg:pt-12 md:pb-4 lg:pb-8',
-    all: 'p-4 py-6 md:p-8 lg:p-12',
+    all: 'p-6 md:p-8 lg:p-12',
   };
 
   const dividers = {
-    top: 'border-t border-light',
-    bottom: 'border-b border-light',
-    both: 'border-y border-light',
+    top: 'border-t border-primary/05',
+    bottom: 'border-b border-primary/05',
+    both: 'border-y border-primary/05',
   };
 
   const displays = {
--- templates/demo-store-neue/src/components/sections/Footer.client.jsx
+++ templates/demo-store-neue/src/components/sections/Footer.client.jsx
@@ -11,7 +11,7 @@ import {footer as mockData} from '~/lib/placeholders';
 export function Footer({menu = mockData}) {
   const styles = {
     footer:
-      'grid items-start w-full grid-flow-row grid-cols-1 gap-6 py-8 px-4 md:px-8 lg:px-12 border-b md:gap-8 lg:gap-12 md:grid-cols-2 lg:grid-cols-4 bg-primary dark:bg-contrast dark:text-primary text-contrast',
+      'grid items-start w-full grid-flow-row grid-cols-1 gap-6 py-8 px-6 md:px-8 lg:px-12 border-b md:gap-8 lg:gap-12 md:grid-cols-2 lg:grid-cols-4 bg-primary dark:bg-contrast dark:text-primary text-contrast',
     section: 'grid gap-4',
     nav: 'grid gap-2 pb-6',
   };
