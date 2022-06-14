7614823fdc17f293a82805b1dcf3277b04a2dc1f
Juan Pablo Prieto <juanpablo.prieto@shopify.com>
2022-06-14 12:34:44 -0700

    remove typo

--- templates/demo-store-neue/src/components/sections/AccountOrderHistory.client.jsx
+++ templates/demo-store-neue/src/components/sections/AccountOrderHistory.client.jsx
@@ -5,7 +5,7 @@ export function AccountOrderHistory({orders}) {
   return (
     <div className="mt-6">
       <div className="grid w-full gap-4 p-4 py-6 md:gap-8 md:p-8 lg:p-12">
-        <h2 className="font-bold text-lead">Order History X</h2>
+        <h2 className="font-bold text-lead">Order History</h2>
         {orders?.length ? <Orders orders={orders} /> : <EmptyOrders />}
       </div>
     </div>
