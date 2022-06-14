0c72b8852c3f98c216a7e1529a2e963f51e94fac
Juan Pablo Prieto <juanpablo.prieto@shopify.com>
2022-06-14 07:14:46 -0700

    remove link for custom font

--- templates/demo-store-neue/src/components/blocks/CustomFontLink.client.jsx
+++ templates/demo-store-neue/src/components/blocks/CustomFontLink.client.jsx
@@ -1,6 +1,6 @@
-import styleSheet from '../../styles/custom-font.css';
+import '../../styles/custom-font.css';
 // import {Head} from '@shopify/hydrogen';
 
 export function CustomFontLink() {
-  return <link rel="stylesheet" href={styleSheet} />;
+  // return <link rel="stylesheet" href={styleSheet} />;
 }
