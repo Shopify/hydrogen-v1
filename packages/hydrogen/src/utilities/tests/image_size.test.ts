97e73cf12a7e9a325544bf2519a4ca1952982220
Anthony Frehner <frehner@users.noreply.github.com>
2022-06-14 15:33:18 -0600

    Try putting the test in the test folder.
    Add tests for no aspect ratio

--- packages/hydrogen/src/utilities/tests/image_size.test.ts
+++ packages/hydrogen/src/utilities/tests/image_size.test.ts
@@ -1,7 +1,7 @@
 import {
   getShopifyImageDimensions,
   type GetShopifyImageDimensionsProps,
-} from './image_size';
+} from '../image_size';
 
 describe(`getShopifyImageDimensions`, () => {
   it(`gives 'loaderOptions' priority`, () => {
@@ -54,6 +54,25 @@ describe(`getShopifyImageDimensions`, () => {
     });
   });
 
+  it(`uses null for 'loaderOptions' if one param is missing and it can't calculate the aspect ratio`, () => {
+    const props = sampleProps();
+    props.loaderOptions!.height = undefined;
+    props.data.width = undefined;
+
+    expect(getShopifyImageDimensions(props)).toEqual({
+      width: 2,
+      height: null,
+    });
+
+    props.loaderOptions!.height = 3;
+    props.loaderOptions!.width = undefined;
+
+    expect(getShopifyImageDimensions(props)).toEqual({
+      height: 3,
+      width: null,
+    });
+  });
+
   it(`calculates the aspect ratio for 'elementProps' if one param is missing`, () => {
     const props = {
       ...sampleProps(),
@@ -72,6 +91,28 @@ describe(`getShopifyImageDimensions`, () => {
     expect(getShopifyImageDimensions(props)).toEqual({
       height: 5,
       width: Math.round(5 * (6 / 7)),
+    });
+  });
+
+  it(`uses null for 'elementProps' if one param is missing and it can't calculate the aspect ratio`, () => {
+    const props = {
+      ...sampleProps(),
+      loaderOptions: undefined,
+    };
+    props.elementProps!.height = undefined;
+    props.data.height = undefined;
+
+    expect(getShopifyImageDimensions(props)).toEqual({
+      width: 4,
+      height: null,
+    });
+
+    props.elementProps!.height = 5;
+    props.elementProps!.width = undefined;
+
+    expect(getShopifyImageDimensions(props)).toEqual({
+      height: 5,
+      width: null,
     });
   });
 });
