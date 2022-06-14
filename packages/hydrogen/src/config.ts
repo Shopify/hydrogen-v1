68b8185e74805a6453e246f01ce69a38988078ef
Fran Dios <fran.dios@shopify.com>
2022-06-14 10:15:58 +0900

    Consolidate config entry point (#1517)
    * Move config utilities to the config entry point
    
    * Update docs
    
    * Changeset
    
    * Fix connectors exported from a non-server component
    
    * Fix imports for demo-store-neue

--- packages/hydrogen/src/config.ts
+++ packages/hydrogen/src/config.ts
@@ -3,3 +3,9 @@ import type {InlineHydrogenConfig} from './types';
 export const defineConfig = (params: InlineHydrogenConfig) => params;
 
 export type {InlineHydrogenConfig as HydrogenConfig};
+
+export {ShopifyServerAnalyticsConnector} from './foundation/Analytics/connectors/Shopify/ServerAnalyticsConnector';
+export {PerformanceMetricsServerAnalyticsConnector} from './foundation/Analytics/connectors/PerformanceMetrics/ServerAnalyticsConnector';
+
+export {CookieSessionStorage} from './foundation/CookieSessionStorage/CookieSessionStorage';
+export {MemorySessionStorage} from './foundation/MemorySessionStorage/MemorySessionStorage';
