c91b85df45dce093d92ff17930c66f778322caff
Juan Pablo Prieto <juanpablo.prieto@shopify.com>
2022-06-15 09:11:46 -0700

    Add ProductInfo accordion open/close icon

--- templates/demo-store-neue/src/components/elements/Icon.jsx
+++ templates/demo-store-neue/src/components/elements/Icon.jsx
@@ -1,16 +1,13 @@
-function Icon({
-  children,
-  className = 'w-5 h-5',
-  fill = 'currentColor',
-  ...props
-}) {
+import clsx from 'clsx';
+
+function Icon({children, className, fill = 'currentColor', ...props}) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 20 20"
       {...props}
       fill={fill}
-      className={className}
+      className={clsx('w-5 h-5', className)}
     >
       {children}
     </svg>
