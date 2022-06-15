11ded1a947a3cf24a84ff570564d1237b433b9f8
Juan Pablo Prieto <juanpablo.prieto@shopify.com>
2022-06-14 19:53:19 -0700

    reorg cards

---
+++ templates/demo-store-neue/src/components/cards/ArticleCard.jsx
@@ -0,0 +1,13 @@
+import {Image, Link} from '@shopify/hydrogen';
+
+export function ArticleCard({blogHandle, article}) {
+  return (
+    <li key={article.id}>
+      <Link to={`/${blogHandle}/${article.handle}`}>
+        <Image data={article.image} className="rounded aspect-[3/2]" />
+        <h2 className="font-medium mt-4">{article.title}</h2>
+        <span className="block mt-1">{article.publishedAt}</span>
+      </Link>
+    </li>
+  );
+}
