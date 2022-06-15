351fea338d86e3b8a09349c6f1e73bdc815eca8f
Bret Little <bret.little@shopify.com>
2022-06-15 17:48:45 -0400

    Fix sanity

--- examples/sanity/src/routes/products/[handle].server.tsx
+++ examples/sanity/src/routes/products/[handle].server.tsx
@@ -1,38 +1,45 @@
-import { gql,useRouteParams, useShopQuery } from '@shopify/hydrogen';
-import groq from 'groq'
-import { useSanityQuery } from '../../../hooks/useSanityQuery';
-
+import {gql, useRouteParams, useShopQuery} from '@shopify/hydrogen';
+import {Product} from '@shopify/hydrogen/dist/esnext/storefront-api-types';
+import groq from 'groq';
+import {useSanityQuery} from '../../../hooks/useSanityQuery';
 
 export default function ProductRoute() {
-  const { handle } = useRouteParams();
-  const { data: sanityProduct } = useSanityQuery({
+  const {handle} = useRouteParams();
+  const {data: sanityProduct} = useSanityQuery({
     query: QUERY_SANITY,
-    params: { slug: handle }
-  })
+    params: {slug: handle},
+  });
   /**
    * Conditionally fetch Shopify data if the product is found in Sanity
    */
-  let storefrontProduct
-  if (sanityProduct?.store?.gid) {
-    const {data: shopifyProduct} = useShopQuery({
+  let storefrontProduct;
+  if (sanityProduct?.store?.gid) {
+    const {data: shopifyProduct} = useShopQuery<{product: Product}>({
       query: QUERY_SHOPIFY,
       variables: {
-        id: sanityProduct?.store?.gid
-      }
-    })
+        id: sanityProduct?.store?.gid,
+      },
+    });
 
-    storefrontProduct = shopifyProduct?.product
+    storefrontProduct = shopifyProduct?.product;
   }
   if (!sanityProduct || !storefrontProduct) {
-    return <main><h1>Sorry, we couldn't find the product you were looking for.</h1></main>
+    return (
+      <main>
+        <h1>Sorry, we couldn't find the product you were looking for.</h1>
+      </main>
+    );
   }
 
-  return (<main>
-    <a href="/">⬅ Home</a>
-    <h1>{storefrontProduct?.title} — {sanityProduct?.store?.vendor}</h1>
-    <p>{storefrontProduct?.description}</p>
-
-  </main>);
+  return (
+    <main>
+      <a href="/">⬅ Home</a>
+      <h1>
+        {storefrontProduct?.title} — {sanityProduct?.store?.vendor}
+      </h1>
+      <p>{storefrontProduct?.description}</p>
+    </main>
+  );
 }
 
 const QUERY_SANITY = groq`
@@ -40,7 +47,7 @@ const QUERY_SANITY = groq`
     _type == "product"
     && store.slug.current == $slug
   ][0]
-`
+`;
 
 const QUERY_SHOPIFY = gql`
   query product($id: ID!) {
@@ -49,6 +56,6 @@ const QUERY_SHOPIFY = gql`
       id
       title
       description
-   }
+    }
   }
-`
+`;
