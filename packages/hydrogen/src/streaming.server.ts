790e5ad369c636664b8d3937f7f977ab37248442
Fran Dios <fran.dios@shopify.com>
2022-06-15 12:31:20 +0900

    Update SSR types

--- packages/hydrogen/src/streaming.server.ts
+++ packages/hydrogen/src/streaming.server.ts
@@ -1,14 +1,12 @@
-import {
-  // @ts-ignore
-  renderToPipeableStream as _ssrRenderToPipeableStream, // Only available in Node context
-  // @ts-ignore
-  renderToReadableStream as _ssrRenderToReadableStream, // Only available in Browser/Worker context
+export {
+  renderToPipeableStream as ssrRenderToPipeableStream, // Only available in Node context
+  renderToReadableStream as ssrRenderToReadableStream, // Only available in Browser/Worker context
 } from 'react-dom/server';
+
 // @ts-ignore
 import {renderToReadableStream as _rscRenderToReadableStream} from '@shopify/hydrogen/vendor/react-server-dom-vite/writer.browser.server';
 // @ts-ignore
 import {createFromReadableStream as _createFromReadableStream} from '@shopify/hydrogen/vendor/react-server-dom-vite';
-import type {Writable} from 'stream';
 
 // From Flight flow types
 type ServerContextJSONValue =
@@ -36,27 +34,6 @@ export const createFromReadableStream = _createFromReadableStream as (
 ) => {
   readRoot: () => JSX.Element;
 };
-
-type StreamOptions = {
-  nonce?: string;
-  bootstrapScripts?: string[];
-  bootstrapModules?: string[];
-  onError?: (error: Error) => void;
-};
-
-export const ssrRenderToPipeableStream = _ssrRenderToPipeableStream as (
-  App: JSX.Element,
-  options: StreamOptions & {
-    onAllReady?: () => void;
-    onShellReady?: () => void;
-    onShellError?: (error: Error) => void;
-  }
-) => {pipe: Writable['pipe']};
-
-export const ssrRenderToReadableStream = _ssrRenderToReadableStream as (
-  App: JSX.Element,
-  options: StreamOptions
-) => Promise<ReadableStream<Uint8Array> & {allReady: Promise<void>}>;
 
 export async function bufferReadableStream(
   reader: ReadableStreamDefaultReader,
