23d59dd513e9edb8cbfe8c2fe81fecde0cb02754
Juan Pablo Prieto <juanpablo.prieto@shopify.com>
2022-06-16 16:25:07 -0700

    refactor placeholders

--- templates/demo-store-neue/src/lib/placeholders.js
+++ templates/demo-store-neue/src/lib/placeholders.js
@@ -1,196 +1,227 @@
-export const hero = {
-  title: {
-    value: 'The Freestyle Collection',
-  },
-  byline: {
-    value:
-      'Light, short and flexible, designed for tricks in terrain parks and halfpipes for all levels of experience.',
-  },
-  cta: {
-    value: 'Shop Now →',
-  },
-  url: {
-    value: '/collections/freestyle',
-  },
-  spread: {
-    reference: {
-      image: {
-        url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/files/spread-01.jpg?v=1654019522',
-        width: 1512,
-        height: 1968,
-      },
-    },
-  },
-  spread_secondary: {
-    reference: {
-      image: {
-        url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/files/spread-02.jpg?v=1654019521',
-        width: 1512,
-        height: 1968,
-      },
-    },
-  },
-  text_color: {
-    value: 'Contrast',
-  },
-};
-
-export const product = {
-  label: 'Limited Edition' /* Metafield */,
-  id: 'gid://shopify/Product/6730850828344',
-  title: 'The Hydrogen',
-  publishedAt: '2021-06-17T18:33:17Z',
-  handle: 'snowboard',
-  description:
-    "Description Our flagship board, ideal for technical terrain and those who dare to go where the chairlift can't take you. The Hydrogen excels in the backcountry making riding out of bounds as easy as resort groomers. New for 2021, the Hydrogen Snowboard has Oxygen Pack inserts giving you more float on the deepest days. Care Guide Clean well after use Wax regularly Specs Weight: 5 lb Length: 4 ft Width: 1 ft Manufactured on: 8/2/2021, 3:30:00 PM Manufactured by: Shopify",
-  priceRange: {
-    minVariantPrice: {
-      amount: '775.0',
-      currencyCode: 'CAD',
-    },
-    maxVariantPrice: {
-      amount: '775.0',
-      currencyCode: 'CAD',
-    },
-  },
-  options: [
+const PLACEHOLDERS = {
+  HEROS: [
+    // primaryHero
     {
-      name: 'Color',
-      values: ['Morning', 'Evening', 'Night'],
+      heading: {value: 'All Mountain All Season'},
+      byline: {
+        value: 'The All New Hydrogen Snowboard Exclusively From Shopify',
+      },
+      cta: {value: 'Shop Now →'},
+      handle: 'freestyle',
+      spread: {
+        reference: {
+          mediaContentType: 'IMAGE',
+          alt: 'Tracks in the snow leading to a person on a mountain top with a red jacket contrasting to an epic blue horizon with a mountain range in the distance.',
+          previewImage: {
+            url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/files/Hydrogen_Hero_Feature_1.jpg?v=1654902468',
+          },
+          id: 'gid://shopify/MediaImage/29259478466616',
+          image: {
+            url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/files/Hydrogen_Hero_Feature_1.jpg?v=1654902468',
+            width: 2500,
+            height: 3155,
+          },
+        },
+      },
+      spreadSecondary: {
+        reference: {
+          mediaContentType: 'IMAGE',
+          alt: 'A snowboarder standing on a mountain top in choppy snow, shows off the back of his snowboard which reads Hydrogen in a cursive script.',
+          previewImage: {
+            url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/files/Hydrogen_Hero_Feature_2.jpg?v=1654902468',
+          },
+          id: 'gid://shopify/MediaImage/29259478499384',
+          image: {
+            url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/files/Hydrogen_Hero_Feature_2.jpg?v=1654902468',
+            width: 2500,
+            height: 3155,
+          },
+        },
+      },
+      height: 'full',
+      top: true,
+      loading: 'eager',
     },
+    // secondaryHero
     {
-      name: 'Size',
-      values: ['154', '158', '160'],
+      heading: {value: 'The Winter 2022 Collection'},
+      byline: {value: 'Just Dropped'},
+      cta: {value: 'Shop Now →'},
+      handle: 'winter-2022',
+      spread: {
+        reference: {
+          mediaContentType: 'IMAGE',
+          alt: 'Three young women in snowboarding attire embracing and laughing while snow falls around them',
+          previewImage: {
+            url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/files/Collection_Feature_Wide.jpg?v=1654902160',
+          },
+          id: 'gid://shopify/MediaImage/29259478302776',
+          image: {
+            url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/files/Collection_Feature_Wide.jpg?v=1654902160',
+            width: 5000,
+            height: 2500,
+          },
+        },
+      },
+      spreadSecondary: null,
+    },
+    // tertiaryHero
+    {
+      heading: {value: 'From the Slopes to the Chalet'},
+      byline: null,
+      cta: {value: 'Shop Now →'},
+      handle: 'backcountry',
+      spread: {
+        reference: {
+          mediaContentType: 'IMAGE',
+          alt: 'A skier hikes up a mountain through the snow with skis over their shoulder.',
+          previewImage: {
+            url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/files/Chalet_Collection_Feature_1.jpg?v=1654902306',
+          },
+          id: 'gid://shopify/MediaImage/29259478368312',
+          image: {
+            url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/files/Chalet_Collection_Feature_1.jpg?v=1654902306',
+            width: 2500,
+            height: 2500,
+          },
+        },
+      },
+      spreadSecondary: {
+        reference: {
+          mediaContentType: 'IMAGE',
+          alt: 'A snow covered lodge is illuminated by lights at night with a dark starry sky and mountain backdrop.',
+          previewImage: {
+            url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/files/Chalet_Collection_Feature_2.jpg?v=1654902306',
+          },
+          id: 'gid://shopify/MediaImage/29259478401080',
+          image: {
+            url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/files/Chalet_Collection_Feature_2.jpg?v=1654902306',
+            width: 2500,
+            height: 2500,
+          },
+        },
+      },
     },
   ],
-  variants: {
-    nodes: [
+  PRODUCT_INFO: [
+    {
+      title: 'Description',
+      content:
+        'We threw snow tires on our core classics... Good for all year round! Named after my favorite football match of the year. Just like any of our joints, dress them up or down...',
+    },
+    {
+      title: 'Size and Fit',
+      content:
+        'We threw snow tires on our core classics... Good for all year round! Named after my favorite football match of the year. Just like any of our joints, dress them up or down...',
+    },
+    {
+      title: 'Delivery and Returns',
+      content:
+        'We threw snow tires on our core classics... Good for all year round! Named after my favorite football match of the year. Just like any of our joints, dress them up or down...',
+    },
+  ],
+  PRODUCT: {
+    label: 'Limited Edition',
+    id: 'gid://shopify/Product/6730850828344',
+    title: 'The Hydrogen',
+    publishedAt: '2021-06-17T18:33:17Z',
+    handle: 'snowboard',
+    description:
+      "Description Our flagship board, ideal for technical terrain and those who dare to go where the chairlift can't take you. The Hydrogen excels in the backcountry making riding out of bounds as easy as resort groomers. New for 2021, the Hydrogen Snowboard has Oxygen Pack inserts giving you more float on the deepest days. Care Guide Clean well after use Wax regularly Specs Weight: 5 lb Length: 4 ft Width: 1 ft Manufactured on: 8/2/2021, 3:30:00 PM Manufactured by: Shopify",
+    priceRange: {
+      minVariantPrice: {
+        amount: '775.0',
+        currencyCode: 'CAD',
+      },
+      maxVariantPrice: {
+        amount: '775.0',
+        currencyCode: 'CAD',
+      },
+    },
+    options: [
       {
-        id: 'gid://shopify/ProductVariant/41007289630776',
-        image: {
-          url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/products/hydrogen-morning.jpg?v=1636146509',
-          altText: 'The Hydrogen snowboard, color Morning',
-          width: 1200,
-          height: 1504,
-        },
-        priceV2: {
-          amount: '775.0',
-          currencyCode: 'CAD',
-        },
-        compareAtPriceV2: {
-          amount: '840.0',
-          currencyCode: 'CAD',
-        },
+        name: 'Color',
+        values: ['Morning', 'Evening', 'Night'],
+      },
+      {
+        name: 'Size',
+        values: ['154', '158', '160'],
       },
     ],
+    variants: {
+      nodes: [
+        {
+          id: 'gid://shopify/ProductVariant/41007289630776',
+          image: {
+            url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/products/hydrogen-morning.jpg?v=1636146509',
+            altText: 'The Hydrogen snowboard, color Morning',
+            width: 1200,
+            height: 1504,
+          },
+          priceV2: {
+            amount: '775.0',
+            currencyCode: 'CAD',
+          },
+          compareAtPriceV2: {
+            amount: '840.0',
+            currencyCode: 'CAD',
+          },
+        },
+      ],
+    },
   },
 };
 
-export const productInfo = [
-  {
-    title: 'Description',
-    content:
-      'We threw snow tires on our core classics... Good for all year round! Named after my favorite football match of the year. Just like any of our joints, dress them up or down...',
-  },
-  {
-    title: 'Size and Fit',
-    content:
-      'We threw snow tires on our core classics... Good for all year round! Named after my favorite football match of the year. Just like any of our joints, dress them up or down...',
-  },
-  {
-    title: 'Delivery and Returns',
-    content:
-      'We threw snow tires on our core classics... Good for all year round! Named after my favorite football match of the year. Just like any of our joints, dress them up or down...',
-  },
-];
+export function withPlaceholder(placeholder, heros) {
+  switch (placeholder) {
+    case 'HEROS': {
+      return heros.map((hero, index) => {
+        if (hero?.title?.value) {
+          return hero;
+        }
 
-export const footer = [
-  {
-    id: 1,
-    title: 'Community',
-    links: [
-      {
-        id: 1,
-        url: 'https://discord.gg/ppSbThrFaS',
-        title: 'Discord',
-      },
-      {
-        id: 2,
-        url: 'https://github.com/Shopify/hydrogen',
-        title: 'Github',
-      },
-      {
-        id: 3,
-        url: 'https://twitter.com/shopifydevs',
-        title: 'Twitter',
-      },
-      {
-        id: 4,
-        url: '/',
-        title: 'Forums',
-      },
-      {
-        id: 5,
-        url: '/',
-        title: 'Support',
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: 'Learning',
-    links: [
-      {
-        id: 6,
-        url: 'https://shopify.dev/custom-storefronts/hydrogen',
-        title: 'End-to-end Tutorial',
-      },
-      {
-        id: 7,
-        url: 'https://shopify.dev/custom-storefronts/hydrogen',
-        title: 'Documentation',
-      },
-      {
-        id: 8,
-        url: '/',
-        title: 'A Book on Hydrogen',
-      },
-      {
-        id: 9,
-        url: 'https://youtube.com',
-        title: 'Popular Youtube Series',
-      },
-      {
-        id: 10,
-        url: 'https://github.com/Shopify/awesome-hydrogen',
-        title: 'Awesome Repo',
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: 'More from Shopify',
-    links: [
-      {
-        id: 1,
-        title: 'Content & Metafields',
-        url: '/',
-      },
-      {
-        id: 2,
-        title: 'Shop Pay',
-        url: '/',
-      },
-      {
-        id: 3,
-        title: 'Audiences',
-        url: '/',
-      },
-      {
-        id: 4,
-        title: 'Markets',
-        url: '/',
-      },
-    ],
-  },
-];
+        // hero placeholder
+        const {
+          id,
+          title,
+          byline,
+          cta,
+          handle,
+          spread,
+          spreadSecondary,
+          height,
+          top,
+        } = PLACEHOLDERS.HEROS[index];
+
+        // prioritize metafield if available, else collection values otherwise placeholder values
+        const byLine =
+          hero?.byLine || hero?.descriptionHtml
+            ? {value: hero.descriptionHtml}
+            : byline;
+
+        const heading =
+          hero?.heading || hero?.title ? {value: hero.title} : title;
+
+        // merge hero placeholder with collection data
+        return {
+          heading,
+          byLine,
+          cta: hero?.cta || cta,
+          handle: hero?.handle || handle,
+          id: hero?.id || id,
+          spread: hero?.spread || spread,
+          spreadSecondary: hero?.spreadSecondary || spreadSecondary,
+          height,
+          top,
+        };
+      });
+    }
+
+    case 'PRODUCT':
+    case 'PRODUCT_INFO':
+      return PLACEHOLDERS[placeholder];
+
+    default:
+      return null;
+  }
+}
