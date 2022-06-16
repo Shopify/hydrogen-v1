a86698c6fcc2547c8eb4bf8210312b41c72911fd
Helen Lin <helen.lin@shopify.com>
2022-06-16 16:11:47 -0700

    add e2e test

--- templates/demo-store/tests/e2e/products.test.js
+++ templates/demo-store/tests/e2e/products.test.js
@@ -25,5 +25,20 @@ describe('products', () => {
 
     const text = await heading.getAttribute('data-test-id');
     expect(text).toBe('gid://shopify/Product/6730850828344');
-  }, 60000);
+  });
+
+  it('can add to cart', async () => {
+    await session.visit('/products/snowboard');
+
+    // Click on add to cart button
+    const [response] = await Promise.all([
+      session.page.waitForResponse(
+        'https://hydrogen-preview.myshopify.com/api/2022-07/graphql.json',
+      ),
+      session.page.locator('text=Add to bag').click(),
+    ]);
+
+    const cartEvent = await response.json();
+    expect(cartEvent.data).not.toBeNull();
+  });
 });
