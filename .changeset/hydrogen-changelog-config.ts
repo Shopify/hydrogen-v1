68e88e20e8ec32bc3ba3e2f6e60b543b1f78e756
Josh Larson <jplhomer@gmail.com>
2022-06-16 07:40:02 -0500

    Revert "Revert "Custom Changeset formatter (#1596)" (#1609)"
    This reverts commit bb9bbf97b4a96d07a84e1032f90e416bf68efc43.

---
+++ .changeset/hydrogen-changelog-config.ts
@@ -0,0 +1,123 @@
+import {ChangelogFunctions} from '@changesets/types';
+// @ts-ignore
+import {config} from 'dotenv';
+import {getInfo, getInfoFromPullRequest} from '@changesets/get-github-info';
+
+config();
+
+const changelogFunctions: ChangelogFunctions = {
+  getDependencyReleaseLine: async (
+    changesets,
+    dependenciesUpdated,
+    options
+  ) => {
+    if (!options.repo) {
+      throw new Error(
+        'Please provide a repo to this changelog generator like this:\n"changelog": ["@changesets/changelog-github", { "repo": "org/repo" }]'
+      );
+    }
+    if (dependenciesUpdated.length === 0) return '';
+
+    const changesetLink = `- Updated dependencies [${(
+      await Promise.all(
+        changesets.map(async (cs) => {
+          if (cs.commit) {
+            let {links} = await getInfo({
+              repo: options.repo,
+              commit: cs.commit,
+            });
+            return links.commit;
+          }
+        })
+      )
+    )
+      .filter((_) => _)
+      .join(', ')}]:`;
+
+    const updatedDepenenciesList = dependenciesUpdated.map(
+      (dependency) => `  - ${dependency.name}@${dependency.newVersion}`
+    );
+
+    return [changesetLink, ...updatedDepenenciesList].join('\n');
+  },
+  getReleaseLine: async (changeset, type, options) => {
+    if (!options || !options.repo) {
+      throw new Error(
+        'Please provide a repo to this changelog generator like this:\n"changelog": ["@changesets/changelog-github", { "repo": "org/repo" }]'
+      );
+    }
+
+    let prFromSummary: number | undefined;
+    let commitFromSummary: string | undefined;
+    let usersFromSummary: string[] = [];
+
+    const replacedChangelog = changeset.summary
+      .replace(/^\s*(?:pr|pull|pull\s+request):\s*#?(\d+)/im, (_, pr) => {
+        let num = Number(pr);
+        if (!isNaN(num)) prFromSummary = num;
+        return '';
+      })
+      .replace(/^\s*commit:\s*([^\s]+)/im, (_, commit) => {
+        commitFromSummary = commit;
+        return '';
+      })
+      .replace(/^\s*(?:author|user):\s*@?([^\s]+)/gim, (_, user) => {
+        usersFromSummary.push(user);
+        return '';
+      })
+      .trim();
+
+    const [firstLine, ...futureLines] = replacedChangelog
+      .split('\n')
+      .map((l) => l.trimRight());
+
+    const links = await (async () => {
+      if (prFromSummary !== undefined) {
+        let {links} = await getInfoFromPullRequest({
+          repo: options.repo,
+          pull: prFromSummary,
+        });
+        if (commitFromSummary) {
+          links = {
+            ...links,
+            commit: `[\`${commitFromSummary}\`](https://github.com/${options.repo}/commit/${commitFromSummary})`,
+          };
+        }
+        return links;
+      }
+      const commitToFetchFrom = commitFromSummary || changeset.commit;
+      if (commitToFetchFrom) {
+        let {links} = await getInfo({
+          repo: options.repo,
+          commit: commitToFetchFrom,
+        });
+        return links;
+      }
+      return {
+        commit: null,
+        pull: null,
+        user: null,
+      };
+    })();
+
+    const users = usersFromSummary.length
+      ? usersFromSummary
+          .map(
+            (userFromSummary) =>
+              `[@${userFromSummary}](https://github.com/${userFromSummary})`
+          )
+          .join(', ')
+      : links.user;
+
+    const prefix = [
+      links.pull === null ? '' : ` (${links.pull})`,
+      users === null ? '' : ` by ${users}`,
+    ].join('');
+
+    return `\n\n- ${firstLine}${prefix ? `${prefix} \n` : ''}\n${futureLines
+      .map((l) => `  ${l}`)
+      .join('\n')}`;
+  },
+};
+
+export default changelogFunctions;
