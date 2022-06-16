e5896a3e20b0bf2760b238e713a7bc04f7e95e2d
Helen Lin <helen.lin@shopify.com>
2022-06-16 10:45:10 -0700

    Clean up full page cache work with `waitUntil` (#1569)

--- packages/hydrogen/src/entry-server.tsx
+++ packages/hydrogen/src/entry-server.tsx
@@ -15,7 +15,7 @@ import type {
   ResolvedHydrogenRoutes,
   RequestHandler,
 } from './types';
-import type {RuntimeContext, RequestHandlerOptions} from './shared-types';
+import type {RequestHandlerOptions} from './shared-types';
 import {Html, applyHtmlHead} from './foundation/Html/Html';
 import {HydrogenResponse} from './foundation/HydrogenResponse/HydrogenResponse.server';
 import {HydrogenRequest} from './foundation/HydrogenRequest/HydrogenRequest.server';
@@ -110,15 +110,6 @@ export const renderHydrogen = (App: any) => {
      * Inject the cache & context into the module loader so we can pull it out for subrequests.
      */
     request.ctx.runtime = context;
-    if (!context?.waitUntil) {
-      const runtimeContext: RuntimeContext = {
-        waitUntil: (fn: Promise<any>) => {
-          setTimeout(() => fn, 0);
-        },
-      };
-
-      request.ctx.runtime = runtimeContext;
-    }
 
     setCache(cache);
 
@@ -150,9 +141,9 @@ export const renderHydrogen = (App: any) => {
       if (cachedResponse) {
         if (isStale(request, cachedResponse)) {
           const lockCacheKey = request.cacheKey(true);
-          const staleWhileRevalidate = async (
-            lockExists: Response | undefined
-          ) => {
+          const staleWhileRevalidatePromise = getItemFromCache(
+            lockCacheKey
+          ).then(async (lockExists: Response | undefined) => {
             if (lockExists) return;
             try {
               // Don't stream when creating a response for cache
@@ -179,15 +170,11 @@ export const renderHydrogen = (App: any) => {
               );
             } catch (e: any) {
               log.error('Cache revalidate error', e);
-            } finally {
-              await deleteItemFromCache(lockCacheKey);
             }
-          };
-          const revalidatingPromise =
-            getItemFromCache(lockCacheKey).then(staleWhileRevalidate);
+          });
 
           // Asynchronously wait for it in workers
-          request.ctx.runtime?.waitUntil(revalidatingPromise);
+          request.ctx.runtime?.waitUntil(staleWhileRevalidatePromise);
         }
 
         return cachedResponse;
@@ -881,11 +868,10 @@ async function cacheResponse(
     if (revalidate) {
       await saveCacheResponse(response, request, chunks);
     } else {
-      request.ctx.runtime?.waitUntil(
-        Promise.resolve(true).then(() =>
-          saveCacheResponse(response, request, chunks)
-        )
+      const cachePutPromise = Promise.resolve(true).then(() =>
+        saveCacheResponse(response, request, chunks)
       );
+      request.ctx.runtime?.waitUntil(cachePutPromise);
     }
   }
 }
@@ -928,5 +914,6 @@ async function saveCacheResponse(
       }),
       response.cache()
     );
+    deleteItemFromCache(request.cacheKey(true));
   }
 }
