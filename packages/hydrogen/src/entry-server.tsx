3d20e92db3402e356c16d0bc5fc4450f9d8c0df5
Josh Larson <josh.larson@shopify.com>
2022-06-17 06:57:08 -0500

    Enable In-Memory Caching by default in Node.js runtimes (#1551)
    * wip
    
    * Add tests for in-memory cache
    
    * Provide exports for easier access to cache adapters
    
    * Add docs and changeset
    
    * Use ArrayBuffer for body to prevent locking issues
    
    * Write more tests and add more functionality
    
    * Ensure non-GET requests are not matched
    
    * Create default variable in constructor
    
    * fix incomplete node caching
    
    * Simplify tests by mocking Date.now()
    
    Co-authored-by: Helen Lin <helen.lin@shopify.com>

--- packages/hydrogen/src/entry-server.tsx
+++ packages/hydrogen/src/entry-server.tsx
@@ -518,6 +518,10 @@ async function runSSR({
   } else if (nodeResponse) {
     const savedChunks = tagOnWrite(nodeResponse);
 
+    nodeResponse.on('finish', () => {
+      cacheResponse(response, request, savedChunks, revalidate);
+    });
+
     const {pipe} = ssrRenderToPipeableStream(AppSSR, {
       nonce,
       bootstrapScripts,
@@ -559,7 +563,6 @@ async function runSSR({
           (response.canStream() || nodeResponse.writableEnded)
         ) {
           postRequestTasks('str', nodeResponse.statusCode, request, response);
-          cacheResponse(response, request, savedChunks, revalidate);
           return;
         }
 
@@ -589,7 +592,6 @@ async function runSSR({
           if (!error) {
             html = assembleHtml({ssrHtml, rscPayload, request, template});
             postRequestTasks('ssr', nodeResponse.statusCode, request, response);
-            cacheResponse(response, request, [html], revalidate);
           }
 
           nodeResponse.write(html);
