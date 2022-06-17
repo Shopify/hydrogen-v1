4072624757330cb84f7c2621c4cc7075047fa3ef
Juan Pablo Prieto <juanpablo.prieto@shopify.com>
2022-06-16 17:12:50 -0700

    fix footer grid when menu is less than 3 columns

--- templates/demo-store-neue/src/components/global/Footer.server.jsx
+++ templates/demo-store-neue/src/components/global/Footer.server.jsx
@@ -8,28 +8,26 @@ import {Section, Heading, FooterMenu, CountrySelector} from '~/components';
 export function Footer({menu}) {
   const {pathname} = useUrl();
   const isHome = pathname === '/';
+  const itemsCounts = menu.items.length + 1; // +1 for the country selector
 
   return (
     <Section
       divider={isHome ? 'none' : 'top'}
       as="footer"
       role="contentinfo"
-      className="
-        grid items-start w-full grid-flow-row grid-cols-1 gap-6
-        py-8 px-6 md:px-8 lg:px-12 border-b md:gap-8 lg:gap-12 md:grid-cols-2 lg:grid-cols-4
-        bg-primary dark:bg-contrast dark:text-primary text-contrast"
+      className={[
+        'grid items-start grid-flow-row w-full gap-6',
+        `py-8 px-6 md:px-8 lg:px-12 border-b md:gap-8 grid-cols-1 lg:gap-12 md:grid-cols-2 lg:grid-cols-${itemsCounts}`,
+        'bg-primary dark:bg-contrast dark:text-primary text-contrast',
+      ].join(' ')}
     >
       <FooterMenu menu={menu} />
-      <section className="grid gap-4 col-[-1]">
+      <section className="grid gap-4 w-full md:max-w-[335px] md:ml-auto">
         <Heading size="lead" className="cursor-default" as="h4">
           Country
         </Heading>
         <CountrySelector />
       </section>
-      <div className="pt-8 opacity-50 md:col-span-2 lg:col-span-4">
-        &copy; {new Date().getFullYear()} / Shopify, Inc. Hydrogen is an MIT
-        Licensed Open Source project. This website is carbon&nbsp;neutral.
-      </div>
     </Section>
   );
 }
