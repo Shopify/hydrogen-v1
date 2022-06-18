0e0f08d6b73cbbf364479e1e4378f2e1190d564a
Matt Seccafien <matt.seccafien@shopify.com>
2022-06-18 04:19:59 +0200

    create new load-config entry instead

--- packages/hydrogen/src/config.ts
+++ packages/hydrogen/src/config.ts
@@ -9,23 +9,3 @@ export {PerformanceMetricsServerAnalyticsConnector} from './foundation/Analytics
 
 export {CookieSessionStorage} from './foundation/CookieSessionStorage/CookieSessionStorage';
 export {MemorySessionStorage} from './foundation/MemorySessionStorage/MemorySessionStorage';
-
-import {createServer} from 'vite';
-import hydrogenVirtualFiles from './framework/plugins/vite-plugin-hydrogen-virtual-files';
-
-export async function loadConfig(root?: string) {
-  const server = await createServer({
-    root,
-    clearScreen: false,
-    server: {middlewareMode: 'ssr'},
-    plugins: [hydrogenVirtualFiles({})],
-  });
-
-  const loaded = await server.ssrLoadModule(
-    'virtual__proxy__hydrogen.config.ts'
-  );
-
-  await server.close();
-
-  return loaded.default;
-}
