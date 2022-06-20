fcde317e3d34e93fd37311af186c36636ca174c5
Benjamin Sehl <ben.sehl@shopify.com>
2022-06-20 10:03:48 -0400

    Updates HomePageSeo component to add titleTemplate (#1642)
    * Updates HomePageSeo component to add titleTemplate
    
    * Adds changeset
    
    * Old graphql schema
    
    * Reverts vendored files, removes Localization from HomepageSeo
    
    * Removes unused declaration

--- packages/hydrogen/src/components/Seo/HomePageSeo.client.tsx
+++ packages/hydrogen/src/components/Seo/HomePageSeo.client.tsx
@@ -4,7 +4,12 @@ import {TitleSeo} from './TitleSeo.client';
 import {DescriptionSeo} from './DescriptionSeo.client';
 import type {HomePage} from './seo-types';
 
-export function HomePageSeo({title, description, url}: HomePage) {
+export function HomePageSeo({
+  title,
+  description,
+  url,
+  titleTemplate,
+}: HomePage) {
   const organizationSchema = {
     '@context': 'http://schema.org',
     '@type': 'Organization',
@@ -21,7 +26,10 @@ export function HomePageSeo({title, description, url}: HomePage) {
 
   return (
     <>
-      <Head>
+      <Head
+        defaultTitle={title ?? ''}
+        titleTemplate={titleTemplate ?? `%s - ${title}`}
+      >
         <meta property="og:url" content={url} />
 
         <script type="application/ld+json">
