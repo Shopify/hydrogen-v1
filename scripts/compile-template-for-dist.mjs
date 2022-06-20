8205beddeab50c21e520a43ed9c51be3a810bc1c
Matt Seccafien <matt.seccafien@shopify.com>
2022-06-20 21:16:30 +0200

    Template Gymnastics (#1650)
    * rename hello-world template
    
    * Compile templates for create-hydrogen-app
    
    Update compile-template.mjs
    
    wip
    
    Seems to work
    
    Update compile-template.mjs
    
    * Update compile_templates.yml
    
    * Also rename source directory
    
    * Update compile-template-for-dist.mjs
    
    * Update compile-template-for-dist.mjs

---
+++ scripts/compile-template-for-dist.mjs
@@ -0,0 +1,31 @@
+import {compileTemplate} from './compile-template.mjs';
+import {resolve} from 'path';
+import fs from 'fs-extra';
+
+(async () => {
+  const [template] = process.argv.slice(2);
+  const destination = resolve(process.cwd(), 'templates', `${template}-js`);
+  const source = resolve(process.cwd(), 'templates');
+
+  await compileTemplate(template, {source, destination});
+  copy(`${source}/${template}`, `${source}/${template}-ts`);
+})();
+
+function copyDir(srcDir, destDir) {
+  fs.mkdirSync(destDir, {recursive: true});
+  for (const file of fs.readdirSync(srcDir)) {
+    const srcFile = resolve(srcDir, file);
+    const destFile = resolve(destDir, file);
+    copy(srcFile, destFile);
+  }
+}
+
+function copy(src, dest) {
+  const stat = fs.statSync(src);
+
+  if (stat.isDirectory()) {
+    copyDir(src, dest);
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+}
