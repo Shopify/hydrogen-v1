fa4c7c95cc2edcb87d18c6b48319b75e8ff06869
Matt Seccafien <matt.seccafien@shopify.com>
2022-06-20 20:43:53 +0200

    Update compile-template-for-dist.mjs

--- scripts/compile-template-for-dist.mjs
+++ scripts/compile-template-for-dist.mjs
@@ -1,12 +1,31 @@
 import {compileTemplate} from './compile-template.mjs';
 import {resolve} from 'path';
-import {rename} from 'fs-extra';
+import fs from 'fs-extra';
 
 (async () => {
   const [template] = process.argv.slice(2);
   const destination = resolve(process.cwd(), 'templates', `${template}-js`);
-  const source = resolve(process.cwd(), 'templates', template);
+  const source = resolve(process.cwd(), 'templates');
 
-  compileTemplate(template, {source, destination});
-  await rename(source, `${source}-ts`);
+  await compileTemplate(template, {source, destination});
+  copy(`${source}/${template}`, `${source}/${template}-ts`);
 })();
+
+function copyDir(srcDir, destDir) {
+  fs.mkdirSync(destDir, {recursive: true});
+  for (const file of fs.readdirSync(srcDir)) {
+    const srcFile = resolve(srcDir, file);
+    const destFile = resolve(destDir, file);
+    copy(srcFile, destFile);
+  }
+}
+
+function copy(src, dest, skipFiles = []) {
+  const stat = fs.statSync(src);
+
+  if (stat.isDirectory()) {
+    copyDir(src, dest, skipFiles);
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+}
