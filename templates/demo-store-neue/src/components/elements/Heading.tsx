73ee8c0c35c0a4f325e3e4f7110687c86616219e
Benjamin Sehl <ben.sehl@shopify.com>
2022-06-19 20:33:51 -0400

    Fix Types for Heading and Text

--- templates/demo-store-neue/src/components/elements/Heading.tsx
+++ templates/demo-store-neue/src/components/elements/Heading.tsx
@@ -14,7 +14,7 @@ export function Heading({
   as?: React.ElementType;
   children: React.ReactNode;
   className?: string;
-  format?: string;
+  format?: boolean;
   size?: 'display' | 'heading' | 'lead' | 'copy';
   width?: 'default' | 'narrow' | 'wide';
 }) {
