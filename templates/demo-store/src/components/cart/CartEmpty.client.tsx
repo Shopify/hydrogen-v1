4914d7cef91bf3dbc1c0a353f9760251799c113d
Daniel Rios Pavia <daniel.riospavia@shopify.com>
2022-06-21 19:42:34 +0200

    New CartEmpty component

--- templates/demo-store/src/components/cart/CartEmpty.client.tsx
+++ templates/demo-store/src/components/cart/CartEmpty.client.tsx
@@ -1,33 +1,33 @@
-import {Link} from '@shopify/hydrogen';
-
-import {Button, Heading, Text} from '~/components';
+import {fetchSync} from '@shopify/hydrogen';
+import {Button, Text, ProductCard, Heading} from '~/components';
+import type {Product} from '@shopify/hydrogen/storefront-api-types';
+import {Suspense} from 'react';
 
 export function CartEmpty({onClose}: {onClose?: () => void}) {
   return (
-    <div className="flex flex-col space-y-7 justify-center items-center md:py-8 md:px-12 px-4 py-6 h-screen">
-      <Heading>Your cart is empty</Heading>
+    <div className="grid gap-1 justify-center items-center md:py-8 md:px-12 px-4 py-6 h-screen">
+      <Text>
+        Looks like you haven&apos;t added anything yet, let&apos;s get you
+        started!
+      </Text>
       <Button onClick={onClose}>Continue shopping</Button>
-      <div className="flex flex-col text-center">
-        <Text>Have an account?</Text>
-        <Text>
-          <Link
-            className="text-orange-600 underline"
-            onClick={onClose}
-            to="/account/register"
-          >
-            Join
-          </Link>{' '}
-          or{' '}
-          <Link
-            className="text-orange-600 underline"
-            onClick={onClose}
-            to="/account/login"
-          >
-            login
-          </Link>{' '}
-          for smoother checkout.
-        </Text>
+      <Heading size="copy">Best sellers</Heading>
+      <div className="grid gap-2 grid-cols-2">
+        <Suspense>
+          <TopProducts />
+        </Suspense>
       </div>
     </div>
+  );
+}
+
+function TopProducts() {
+  const products: Product[] = fetchSync('/api/bestSellers').json();
+  return (
+    <>
+      {products.map((product) => (
+        <ProductCard product={product} key={product.id} />
+      ))}
+    </>
   );
 }
