a8e51bcbebf3c3505be90a66015ec93e19895bd9
Benjamin Sehl <ben.sehl@shopify.com>
2022-06-21 02:17:44 -0400

    Improves styling of Collection Cards

--- templates/demo-store/src/components/sections/FeaturedCollections.tsx
+++ templates/demo-store/src/components/sections/FeaturedCollections.tsx
@@ -24,20 +24,22 @@ export function FeaturedCollections({
           if (!collection?.image) {
             return null;
           }
+          // TODO: Refactor to use CollectionCard
           return (
             <Link key={collection.id} to={`/collections/${collection.handle}`}>
               <div className="grid gap-4">
-                {collection?.image && (
-                  <Image
-                    className="rounded shadow-border overflow-clip inline-block aspect-[5/4] md:aspect-[3/2] object-cover"
-                    width={500}
-                    height={400}
-                    widths={[400, 500, 600, 700, 800, 900]}
-                    sizes="(max-width: 32em) 100vw, 33vw"
-                    alt={`Image of ${collection.title}`}
-                    data={collection.image}
-                  />
-                )}
+                <div className="card-image bg-primary/5 aspect-[3/2]">
+                  {collection?.image && (
+                    <Image
+                      alt={`Image of ${collection.title}`}
+                      data={collection.image}
+                      height={400}
+                      sizes="(max-width: 32em) 100vw, 33vw"
+                      width={600}
+                      widths={[400, 500, 600, 700, 800, 900]}
+                    />
+                  )}
+                </div>
                 <Heading size="copy">{collection.title}</Heading>
               </div>
             </Link>
