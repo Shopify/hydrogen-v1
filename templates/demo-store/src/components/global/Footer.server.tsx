4858eccd35fc209e94735c008efb54f645fa7469
Daniel Rios Pavia <daniel.riospavia@shopify.com>
2022-06-21 12:34:47 +0200

    Fix footer and countrySelect showing white space

--- templates/demo-store/src/components/global/Footer.server.tsx
+++ templates/demo-store/src/components/global/Footer.server.tsx
@@ -26,7 +26,7 @@ export function Footer({menu}: {menu?: EnhancedMenu}) {
       role="contentinfo"
       className={`grid min-h-[25rem] items-start grid-flow-row w-full gap-6 py-8 px-6 md:px-8 lg:px-12 
         border-b md:gap-8 lg:gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-${itemsCount}
-        bg-primary dark:bg-contrast dark:text-primary text-contrast`}
+        bg-primary dark:bg-contrast dark:text-primary text-contrast overflow-hidden`}
     >
       <FooterMenu menu={menu} />
       <section className="grid gap-4 w-full md:max-w-[335px] md:ml-auto">
