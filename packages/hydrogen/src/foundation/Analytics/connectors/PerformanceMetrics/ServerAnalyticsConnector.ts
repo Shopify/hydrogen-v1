f7158b50366bd48af74aa5229c55d7e850a68448
Helen Lin <helen.lin@shopify.com>
2022-06-21 10:01:34 -0700

    missed a file

--- packages/hydrogen/src/foundation/Analytics/connectors/PerformanceMetrics/ServerAnalyticsConnector.ts
+++ packages/hydrogen/src/foundation/Analytics/connectors/PerformanceMetrics/ServerAnalyticsConnector.ts
@@ -1,35 +1,10 @@
 import {log} from '../../../../utilities/log';
 
 export const PerformanceMetricsServerAnalyticsConnector = {
-  request(
-    requestUrl: string,
-    requestHeader: Headers,
-    data?: any,
-    contentType?: string
-  ): Promise<any> {
-    const url = new URL(requestUrl);
-    if (url.search === '?performance' && contentType === 'json') {
-      const initTime = new Date().getTime();
-
-      return fetch('https://monorail-edge.shopifysvc.com/v1/produce', {
-        method: 'post',
-        headers: {
-          'content-type': 'text/plain',
-          'x-forwarded-for': requestHeader.get('x-forwarded-for') || '',
-          'user-agent': requestHeader.get('user-agent') || '',
-        },
-        body: JSON.stringify({
-          schema_id: 'hydrogen_buyer_performance/2.0',
-          payload: data,
-          metadata: {
-            event_created_at_ms: initTime,
-            event_sent_at_ms: new Date().getTime(),
-          },
-        }),
-      }).catch((err) => {
-        log.error(err);
-      });
-    }
+  request(): Promise<any> {
+    log.warn(
+      'PerformanceMetricsServerAnalyticsConnector has been removed - please remove its reference from hydrogen.config.js'
+    );
     return Promise.resolve(true);
   },
 };
