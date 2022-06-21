80bd806485d2fe6f8a764116e3c0255e44211c99
Bret Little <bret.little@shopify.com>
2022-06-21 15:33:43 -0400

    Take out the trash

--- shopify.config.js
+++
@@ -1,9 +0,0 @@
-// This is only here to trick Oxygen into thinking this is a valid deployment.
-
-export default {
-  defaultLanguageCode: 'en',
-  defaultCountryCode: 'US',
-  storeDomain: 'hydrogen-preview.myshopify.com',
-  storefrontToken: '174af69ea23ab0c1d7191f13fd82ea3c',
-  storefrontApiVersion: 'unstable',
-};
