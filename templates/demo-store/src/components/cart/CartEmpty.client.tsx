c9703aba441ebb30376c6f8dc542a468e8953750
Daniel Rios <daniel.riospavia@shopify.com>
2022-06-21 22:42:01 +0200

    [Demo store neue] New empty cart (#1698)

--- templates/demo-store/src/components/cart/CartEmpty.client.tsx
+++ templates/demo-store/src/components/cart/CartEmpty.client.tsx
@@ -1,33 +1,70 @@
-import {Link} from '@shopify/hydrogen';
-
-import {Button, Heading, Text} from '~/components';
+import {useRef} from 'react';
+import {useScroll} from 'react-use';
+import {fetchSync} from '@shopify/hydrogen';
+import {Button, Text, ProductCard, Heading, Skeleton} from '~/components';
+import type {Product} from '@shopify/hydrogen/storefront-api-types';
+import {Suspense} from 'react';
 
 export function CartEmpty({onClose}: {onClose?: () => void}) {
+  const scrollRef = useRef(null);
+  const {y} = useScroll(scrollRef);
+
   return (
-    <div className="flex flex-col space-y-7 justify-center items-center md:py-8 md:px-12 px-4 py-6 h-screen">
-      <Heading>Your cart is empty</Heading>
-      <Button onClick={onClose}>Continue shopping</Button>
-      <div className="flex flex-col text-center">
-        <Text>Have an account?</Text>
-        <Text>
-          <Link
-            className="text-orange-600 underline"
-            onClick={onClose}
-            to="/account/register"
-          >
-            Join
-          </Link>{' '}
-          or{' '}
-          <Link
-            className="text-orange-600 underline"
-            onClick={onClose}
-            to="/account/login"
-          >
-            login
-          </Link>{' '}
-          for smoother checkout.
+    <div
+      ref={scrollRef}
+      className={`grid content-start gap-4 px-6 pb-8 transition overflow-y-scroll md:gap-12 md:px-12 h-screen-no-nav md:pb-12 ${
+        y > 0 && 'border-t'
+      }`}
+    >
+      <section className="grid gap-6">
+        <Text format>
+          Looks like you haven&rsquo;t added anything yet, let&rsquo;s get you
+          started!
         </Text>
-      </div>
+        <div>
+          <Button onClick={onClose}>Continue shopping</Button>
+        </div>
+      </section>
+      <section className="grid gap-8 pt-4">
+        <Heading format size="copy">
+          Shop Best Sellers
+        </Heading>
+        <div className="grid grid-cols-2 gap-x-6 gap-y-8">
+          <Suspense fallback={<Loading />}>
+            <TopProducts onClose={onClose} />
+          </Suspense>
+        </div>
+      </section>
     </div>
+  );
+}
+
+function TopProducts({onClose}: {onClose?: () => void}) {
+  const products: Product[] = fetchSync('/api/bestSellers').json();
+
+  if (products.length === 0) {
+    return <Text format>No products found.</Text>;
+  }
+
+  return (
+    <>
+      {products.map((product) => (
+        <ProductCard product={product} key={product.id} onClick={onClose} />
+      ))}
+    </>
+  );
+}
+
+function Loading() {
+  return (
+    <>
+      {[...new Array(4)].map((_, i) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <div key={i} className="grid gap-2">
+          <Skeleton className="aspect-[3/4]" />
+          <Skeleton className="w-32 h-4" />
+        </div>
+      ))}
+    </>
   );
 }
