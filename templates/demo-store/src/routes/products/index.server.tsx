1b374067491e12faafc60112b3712f2d4d3654e2
Fran Dios <fran.dios@shopify.com>
2022-06-22 11:56:20 +0900

    Add suspense to collections and products index pages

--- templates/demo-store/src/routes/products/index.server.tsx
+++ templates/demo-store/src/routes/products/index.server.tsx
@@ -1,3 +1,4 @@
+import {Suspense} from 'react';
 import {
   useShopQuery,
   gql,
@@ -12,9 +13,22 @@ import {PAGINATION_SIZE} from '~/lib/const';
 import {ProductGrid, PageHeader, Section} from '~/components';
 import {Layout} from '~/components/index.server';
 import type {Collection} from '@shopify/hydrogen/storefront-api-types';
-import {Suspense} from 'react';
 
 export default function AllProducts() {
+  return (
+    <Layout>
+      <Seo type="page" data={{title: 'All Products'}} />
+      <PageHeader heading="All Products" variant="allCollections" />
+      <Section>
+        <Suspense>
+          <AllProductsGrid />
+        </Suspense>
+      </Section>
+    </Layout>
+  );
+}
+
+function AllProductsGrid() {
   const {
     language: {isoCode: languageCode},
     country: {isoCode: countryCode},
@@ -33,24 +47,11 @@ export default function AllProducts() {
   const products = data.products;
 
   return (
-    <Layout>
-      <Suspense>
-        <Seo
-          type="page"
-          data={{
-            title: 'All Products',
-          }}
-        />
-      </Suspense>
-      <PageHeader heading="All Products" variant="allCollections" />
-      <Section>
-        <ProductGrid
-          key="products"
-          url={`/products?country=${countryCode}`}
-          collection={{products} as Collection}
-        />
-      </Section>
-    </Layout>
+    <ProductGrid
+      key="products"
+      url={`/products?country=${countryCode}`}
+      collection={{products} as Collection}
+    />
   );
 }
 
