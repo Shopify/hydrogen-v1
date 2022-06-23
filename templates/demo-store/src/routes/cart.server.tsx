d44a5aae927999e134a8c7ad8c6d7f3633edbc3e
Daniel Rios Pavia <daniel.riospavia@shopify.com>
2022-06-23 09:56:23 +0200

    Fix cart routes

--- templates/demo-store/src/routes/cart.server.tsx
+++ templates/demo-store/src/routes/cart.server.tsx
@@ -6,10 +6,8 @@ export default function Cart() {
   return (
     <Layout>
       <Seo type="page" data={{title: 'Cart'}} />
-      <div className="w-full mx-auto max-w-7xl xl:-translate-x-12">
-        <PageHeader heading="Your Cart" />
-      </div>
-      <Section padding="x">
+      <PageHeader heading="Your Cart" />
+      <Section>
         <CartDetails layout="page" />
       </Section>
     </Layout>
