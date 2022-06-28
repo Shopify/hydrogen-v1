d048fa25c1ad0e88d60b9811fac5a02c7f420f40
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-06-27 20:49:16 -0700

    load translation dynamically

--- examples/i18n/src/components/TranslationProvider.client.jsx
+++ examples/i18n/src/components/TranslationProvider.client.jsx
@@ -4,13 +4,26 @@ import {I18nextProvider} from 'react-i18next';
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import resourcesToBackend from 'i18next-resources-to-backend';
 
-// i18n translations
+// Bundle the default translation
 import en from '../assets/locales/en/translation.json';
-import es from '../assets/locales/es/translation.json';
-import de from '../assets/locales/de/translation.json';
 
-i18n.use(LanguageDetector).use(initReactI18next);
+i18n
+  // load other translations dynamically
+  .use(
+    resourcesToBackend((language, namespace, callback) => {
+      import(`../assets/locales/${language}/${namespace}.json`)
+        .then((resources) => {
+          callback(null, resources);
+        })
+        .catch((error) => {
+          callback(error, null);
+        });
+    })
+  )
+  .use(LanguageDetector)
+  .use(initReactI18next);
 
 export function TranslationProvider({children}) {
   const {language} = useLocalization();
@@ -27,22 +40,20 @@ export function TranslationProvider({children}) {
     if (!cachedLang) {
       localStorage.setItem('i18nextLng', defaultLang);
     }
-
     // initialize i18next
     i18n.init(
       {
+        partialBundledLanguages: true, // This allows some resources to be set on init while others after
         fallbackLng: defaultLang,
         debug: false,
         react: {useSuspense: true},
         ns: ['translation'],
         defaultNS: 'translation',
         interpolation: {
-          escapeValue: false, // not needed for react as it escapes by default
+          escapeValue: false,
         },
         resources: {
           en: {translation: en},
-          es: {translation: es},
-          de: {translation: de},
         },
       },
       () => {
