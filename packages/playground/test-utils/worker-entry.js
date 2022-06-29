c0c8e09cc0cdde1a912629dae402383ee8663367
Fran Dios <fran.dios@shopify.com>
2022-06-29 16:57:19 +0900

    Use new public entry point in e2e tests

--- packages/playground/test-utils/worker-entry.js
+++ packages/playground/test-utils/worker-entry.js
@@ -1,43 +1,46 @@
 import {getAssetFromKV} from '@cloudflare/kv-asset-handler';
-import {isAsset} from '@shopify/hydrogen/virtual/assets';
+import {
+  isAsset,
+  handleRequest,
+  indexTemplate,
+} from '@shopify/hydrogen/platforms/virtual';
 
-export default function setup({handleRequest, indexTemplate}) {
-  // Mock Oxygen global
-  globalThis.Oxygen = {env: globalThis};
+// Mock Oxygen global
+globalThis.Oxygen = {env: globalThis};
 
-  async function handleAsset(url, event) {
-    const response = await getAssetFromKV(event, {});
+async function handleAsset(url, event) {
+  const response = await getAssetFromKV(event, {});
 
-    if (response.status < 400) {
-      const filename = url.pathname.split('/').pop();
+  if (response.status < 400) {
+    const filename = url.pathname.split('/').pop();
 
-      const maxAge =
-        filename.split('.').length > 2
-          ? 31536000 // hashed asset, will never be updated
-          : 86400; // favicon and other public assets
+    const maxAge =
+      filename.split('.').length > 2
+        ? 31536000 // hashed asset, will never be updated
+        : 86400; // favicon and other public assets
 
-      response.headers.append('cache-control', `public, max-age=${maxAge}`);
-    }
-
-    return response;
+    response.headers.append('cache-control', `public, max-age=${maxAge}`);
   }
 
-  async function handleEvent(event) {
-    try {
-      const url = new URL(event.request.url);
-
-      if (isAsset(url.pathname)) {
-        return await handleAsset(url, event);
-      }
-
-      return await handleRequest(event.request, {
-        indexTemplate,
-        cache: caches.default,
-        context: event,
-      });
-    } catch (error) {
-      return new Response(error.message || error.toString(), {status: 500});
-    }
-  }
-  addEventListener('fetch', (event) => event.respondWith(handleEvent(event)));
+  return response;
 }
+
+async function handleEvent(event) {
+  try {
+    const url = new URL(event.request.url);
+
+    if (isAsset(url.pathname)) {
+      return await handleAsset(url, event);
+    }
+
+    return await handleRequest(event.request, {
+      indexTemplate,
+      cache: caches.default,
+      context: event,
+    });
+  } catch (error) {
+    return new Response(error.message || error.toString(), {status: 500});
+  }
+}
+
+addEventListener('fetch', (event) => event.respondWith(handleEvent(event)));
