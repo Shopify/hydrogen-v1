52783d634f607687ed18b8d278e05da4f097e7d8
Daniel Rios <daniel.riospavia@shopify.com>
2022-07-15 15:13:18 +0200

    Update packages/hydrogen/src/utilities/locale/locale.ts
    Co-authored-by: Ren Chaturvedi <63201605+rennyG@users.noreply.github.com>

--- packages/hydrogen/src/utilities/locale/locale.ts
+++ packages/hydrogen/src/utilities/locale/locale.ts
@@ -2,7 +2,7 @@ import {CountryCode, LanguageCode} from '../../storefront-api-types';
 
 /**
  * Calculates locale based on provided language and countryCode
- * 1. If language is extended with region, hyphenate and use as locale
+ * 1. If language is extended with region, then hyphenate and use as locale
  * 2. Else merge language and countryCode
  */
 export function getLocale(
