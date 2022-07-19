1181f5cfbcb5981d9b914ac7088adec20d17e0f5
Helen Lin <helen.lin@shopify.com>
2022-07-19 12:15:26 -0700

    fix cloudflare deploy

--- templates/demo-store/hydrogen.config.ts
+++ templates/demo-store/hydrogen.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
     defaultLanguageCode: 'EN',
     storeDomain:
       // @ts-ignore
-      Oxygen.env.SHOPIFY_STORE_DOMAIN || 'hydrogen-preview.myshopify.com',
+      Oxygen?.env?.SHOPIFY_STORE_DOMAIN || 'hydrogen-preview.myshopify.com',
     storefrontToken:
       // @ts-ignore
-      Oxygen.env.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN ||
+      Oxygen?.env?.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN ||
       '3b580e70970c4528da70c98e097c2fa0',
     storefrontApiVersion: '2022-07',
   }),
