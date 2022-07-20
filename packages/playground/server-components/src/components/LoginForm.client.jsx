a668159a54acc276bfba80ad35c3151c61122d1b
Bret Little <bret.little@shopify.com>
2022-07-20 10:31:07 -0400

    Fix

--- packages/playground/server-components/src/components/LoginForm.client.jsx
+++ packages/playground/server-components/src/components/LoginForm.client.jsx
@@ -50,7 +50,7 @@ export function LoginForm({error}) {
         }
       }}
     >
-      {(loading) => (
+      {({loading}) => (
         <>
           <input type="hidden" name="action" value="login" />
           <div>
