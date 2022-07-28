9f287a2e5a0e6446dd929f6f0dc1cbca227c38c3
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-07-28 07:25:51 -0700

    Update examples/admin-api-todo/src/components/TodoList.server.jsx
    Co-authored-by: Ren Chaturvedi <63201605+rennyG@users.noreply.github.com>

--- examples/admin-api-todo/src/components/TodoList.server.jsx
+++ examples/admin-api-todo/src/components/TodoList.server.jsx
@@ -6,7 +6,7 @@ import {SHOP_METAFIELDS_QUERY} from '../graphql/queries';
 export function TodoList() {
   /*
     Fetch todos/metafields synchronously from the Admin API
-    just for demonstration purposes - (Should be fetched via storefront API)
+    For demonstration purposes only. Should be fetched via the Storefront API)
   */
   const {data, error = null} = fetchTodosSync();
 
