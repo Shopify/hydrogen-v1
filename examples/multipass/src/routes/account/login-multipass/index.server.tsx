c9f172b951dd226aba54f90b8927c70dd7f41db6
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-07-29 15:49:22 -0700

    update copy

--- examples/multipass/src/routes/account/login-multipass/index.server.tsx
+++ examples/multipass/src/routes/account/login-multipass/index.server.tsx
@@ -7,8 +7,22 @@ export default function LoginMultipass() {
     <>
       <h1>Sign in with multipass</h1>
       <p>
-        If a user exists with this email we log them in, otherwise we create an
-        account and log them in.
+        If a user exists with the entered email we log them in, otherwise we
+        create an account and log them in.
+      </p>
+      <p>
+        <b>
+          Notice: In this form, this is NOT a good practice. This would allow
+          any user to access another's user account by simply entering their
+          email.
+        </b>
+      </p>
+      <p>
+        <b>
+          This mechanism should not be exposed to the user. Instead it should be
+          only be used as a middleware when another auth system has already
+          validated the user attempting to access a given account.
+        </b>
       </p>
       <LoginFormMultipass />
       <br />
--- examples/multipass/src/routes/index.server.tsx
+++ examples/multipass/src/routes/index.server.tsx
@@ -10,18 +10,73 @@ export default function Home() {
   return (
     <div>
       <h1>Multipass Demo</h1>
+      <p>
+        This repo demonstrates how to seamlessly integrate &nbsp;
+        <a
+          target="_blank"
+          href="https://www.shopify.com/plus?shpxid=4b620f16-EA6D-46D2-8851-DB05208459BB"
+        >
+          Shopify Plus
+        </a>
+        &nbsp;
+        <a target="_blank" href="https://shopify.dev/api/multipass">
+          multipass
+        </a>{' '}
+        feature with Hydrogen's customer &nbsp;
+        <a
+          target="_blank"
+          href="https://https://shopify.dev/custom-storefronts/hydrogen/framework/sessions"
+        >
+          session
+        </a>
+        &nbsp; and checkout session.
+      </p>
+
+      <h4>With multipass you can:</h4>
+      <ol>
+        <li>
+          Persist the customer state across the hydrogen and the checkout flows.
+          eg — stay logged in / out
+        </li>
+        <li>
+          Automatically log in or create new accounts based on a external site's
+          user profile.
+        </li>
+        <li>
+          Log in or create accounts by leveraging a third-party auth service
+          such as&nbsp;
+          <a
+            target="_blank"
+            href="https://developers.google.com/identity/gsi/web/guides/overview"
+          >
+            Google Sign in
+          </a>
+          .
+        </li>
+      </ol>
+      <hr />
       <br />
+      <h5>Menu</h5>
       {customerAccessToken ? (
-        <Link to="/account">Account</Link>
+        <Link to="/account">View account</Link>
       ) : (
         <div>
-          <Link to="/account/login">Login</Link>
+          <div>
+            <Link to="/account/login">Login</Link> —{' '}
+            <small>classic hydrogen login flow (email and password)</small>
+          </div>
           <br />
-          <Link to="/account/login-multipass">Login (multipass)</Link>
+          <div>
+            <Link to="/account/login-multipass">Login (multipass)</Link> —{' '}
+            <small>login via multipass (email only)</small>
+          </div>
           <br />
-          <Link to="/account/login-multipass-google">
-            Login (multipass google)
-          </Link>
+          <div>
+            <Link to="/account/login-multipass-google">
+              Login (multipass google)
+            </Link>{' '}
+            — <small>login via multipass (Google profile)</small>
+          </div>
         </div>
       )}
       <hr />
