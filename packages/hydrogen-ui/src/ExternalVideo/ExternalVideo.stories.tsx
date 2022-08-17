a0df8381eaf615505d398b31b941dcfc41d80e33
Anthony Frehner <frehner@users.noreply.github.com>
2022-08-17 16:00:35 -0600

    simplify story props

--- packages/hydrogen-ui/src/ExternalVideo/ExternalVideo.stories.tsx
+++ packages/hydrogen-ui/src/ExternalVideo/ExternalVideo.stories.tsx
@@ -14,22 +14,18 @@ const Template: Story<ExternalVideoProps> = (props) => (
 
 export const Youtube = Template.bind({});
 Youtube.args = {
-  data: {
-    ...getExternalVideoData({
-      embedUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
-      host: 'YOUTUBE',
-    }),
-  },
+  data: getExternalVideoData({
+    embedUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
+    host: 'YOUTUBE',
+  }),
 };
 
 export const Vimeo = Template.bind({});
 Vimeo.args = {
-  data: {
-    ...getExternalVideoData({
-      embedUrl: 'https://player.vimeo.com/video/375468729?h=d063a6fe74',
-      host: 'VIMEO',
-    }),
-  },
+  data: getExternalVideoData({
+    embedUrl: 'https://player.vimeo.com/video/375468729?h=d063a6fe74',
+    host: 'VIMEO',
+  }),
 };
 
 function getExternalVideoData(
