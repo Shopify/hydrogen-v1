a5405e9f8f7bcc411689af316e1d98597ce8b95f
Anthony Frehner <frehner@users.noreply.github.com>
2022-08-23 16:30:16 -0600

    Migrate Image to hui, add stories and test (#2032)
    * Migrate Image to hui, add stories and test
    
    Fix import path for ExternalVideo.test
    
    Add image-size.ts and update import paths of existing components to point to it
    
    Add a video stories file
    
    Update video to latest from hydrogen, and update video's tests
    
    Add hui's tests to the main script so they're run in CI
    
    * Add more options to the image story

--- packages/hydrogen-ui/src/index.ts
+++ packages/hydrogen-ui/src/index.ts
@@ -1,3 +1,4 @@
 export {ShopifyProvider, useShop} from './ShopifyProvider.js';
+export {Image} from './Image.js';
 export {ExternalVideo} from './ExternalVideo.js';
 export {Video} from './Video.js';
