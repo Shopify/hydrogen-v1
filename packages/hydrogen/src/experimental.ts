a385d9995f427049bf29c04b86c7370e1284b33c
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-09-16 15:54:58 -0700

    update experimental export

--- packages/hydrogen/src/experimental.ts
+++ packages/hydrogen/src/experimental.ts
@@ -1,4 +1,6 @@
 export {Form} from './foundation/Form/Form.client.js';
 export {useFlashSession} from './foundation/useSession/useSession.js';
-export {Script, loadScript, useLoadScript} from './foundation/Script/index.js';
+export {Script} from './foundation/Script/Script.client.js';
+export {loadScript} from './foundation/Script/loadScript.js';
+export {useScript} from './foundation/Script/useScript.client.js';
 export * from './foundation/Script/types.js';
