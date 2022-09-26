9528a0113b0d3c768abc3134c8e8fcbecaee929c
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-09-26 13:01:39 -0700

    remove experimental

--- packages/hydrogen/src/experimental.ts
+++ packages/hydrogen/src/experimental.ts
@@ -1,6 +1,2 @@
 export {Form} from './foundation/Form/Form.client.js';
 export {useFlashSession} from './foundation/useSession/useSession.js';
-export {Script} from './foundation/Script/Script.client.js';
-export {loadScript} from './foundation/Script/loadScript.js';
-export {useScript} from './foundation/Script/useScript.client.js';
-export * from './foundation/Script/types.js';
