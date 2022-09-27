624cb4d6ba6e9cfe7a15580b66b0d57a77786eea
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-09-27 16:05:00 -0700

    fix Script error in App.server.tsx, update partytown snippet

--- packages/playground/server-components/src/App.server.jsx
+++ packages/playground/server-components/src/App.server.jsx
@@ -1,15 +1,14 @@
 import renderHydrogen from '@shopify/hydrogen/entry-server';
 import {partytownSnippet} from '@builder.io/partytown/integration';
-// import {Partytown} from '@builder.io/partytown/react';
-
+// import {Script} from '@shopify/hydrogen/experimental';
 import {
   Route,
   Router,
   FileRoutes,
   ShopifyProvider,
   useRequestContext,
+  Script,
 } from '@shopify/hydrogen';
-import {Script} from '@shopify/hydrogen/experimental';
 import {Suspense} from 'react';
 import Custom1 from './customRoutes/custom1.server';
 import Custom2 from './customRoutes/custom2.server';
@@ -39,62 +38,6 @@ export default renderHydrogen(({request, response}) => {
     <Suspense fallback={'Loading...'}>
       {/* Add to App.server.jsx */}
 
-      {/* partytown config */}
-      <Script
-        load="beforeHydration"
-        id="partytown-config"
-        suppressHydrationWarning={true}
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.partytown = {
-              forward: ['forwardedTestFn'],
-              resolveUrl: function(url, location, type) {
-                // Some 3rd party libs/resources like https://www.googletagmanager.com/gtm.js
-                // require a reverse proxy to handle CORS via when loaded via Web Worker
-                const isScriptReq = type === 'script';
-                const isProxyReq = url.href.includes('/reverse-proxy');
-                const isCorsReq = url.href.includes('cors=true') || url.href.includes('gtm.js')
-
-                if (isScriptReq && isCorsReq && !isProxyReq) {
-                  // console.log('🎉 Loading partytown script via CORS reverse proxy:', url.href)
-                  const proxyUrl = new URL(location.origin + '/reverse-proxy');
-                  proxyUrl.searchParams.append('libUrl', url.href);
-                  return proxyUrl;
-                }
-                return url;
-              }
-            };
-          `,
-        }}
-      />
-
-      {/*  */}
-      {/* <Partytown
-        id="partytown-snippet"
-        forward={['forwardedTestFn', 'dataLayer.push']}
-        debug={true}
-        resolveUrl={(url, location, type) => {
-          console.log('resolveUrl: url', url);
-          // Some 3rd party libs/resources like https://www.googletagmanager.com/gtm.js
-          // require a reverse proxy to handle CORS via when loaded via Web Worker
-          const isScriptReq = type === 'script';
-          const isProxyReq = url.href.includes('/reverse-proxy');
-          const isCorsReq = url.href.includes('cors=true');
-
-          if (isScriptReq && isCorsReq && !isProxyReq) {
-            console.log(
-              '🎉 Loading partytown script via CORS reverse proxy:',
-              url.href
-            );
-            const proxyUrl = new URL(location.origin + '/reverse-proxy');
-            url.searchParams.delete('cors');
-            proxyUrl.searchParams.append('libUrl', url.href);
-            return proxyUrl;
-          }
-          return url;
-        }}
-      /> */}
-
       <ShopifyProvider>
         <Router>
           <Route path="/custom1" page={<Custom1 />} />
@@ -107,12 +50,29 @@ export default renderHydrogen(({request, response}) => {
           <FileRoutes />
           <LazyRoute />
         </Router>
-      </ShopifyProvider>
 
-      {/* load partytown lib/runtime last so that it can pick type="text/partytown" scripts */}
-      <Script id="partytown-snippet" load="onIdle">
-        {partytownSnippet()}
-      </Script>
+        {/* load partytown lib/runtime last so that it can pick type="text/partytown" scripts */}
+        <Script id="partytown-snippet" load="onIdle">
+          {partytownSnippet({
+            forward: ['forwardedTestFn'],
+            resolveUrl(url, location, type) {
+              // Some 3rd party libs/resources like https://www.googletagmanager.com/gtm.js
+              // require a reverse proxy to handle CORS via when loaded via Web Worker
+              const isScriptReq = type === 'script';
+              const isProxyReq = url.href.includes('/reverse-proxy');
+              const isCorsReq =
+                url.href.includes('cors=true') || url.href.includes('gtm.js');
+
+              if (isScriptReq && isCorsReq && !isProxyReq) {
+                const proxyUrl = new URL(location.origin + '/reverse-proxy');
+                proxyUrl.searchParams.append('libUrl', url.href);
+                return proxyUrl;
+              }
+              return url;
+            },
+          })}
+        </Script>
+      </ShopifyProvider>
     </Suspense>
   );
 });
