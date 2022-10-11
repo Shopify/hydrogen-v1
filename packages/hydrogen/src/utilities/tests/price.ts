32d4fdc5087ac6cd03c1616ec199e868bd0d33e1
wmw_drewgarratt <drew.garratt@wemakewebsites.com>
2022-10-11 14:27:54 +0100

    fix(2233): removed fake dep
    - Removed faker dep from utilities/tests/price.ts
    - Added hard coded fallback for undeclared price.amount

--- packages/hydrogen/src/utilities/tests/price.ts
+++ packages/hydrogen/src/utilities/tests/price.ts
@@ -1,10 +1,9 @@
 // eslint-disable-next-line node/no-extraneous-import
-import faker from 'faker';
 import {CurrencyCode, MoneyV2} from '../../storefront-api-types.js';
 
 export function getPrice(price: Partial<MoneyV2> = {}) {
   return {
     currencyCode: price.currencyCode ?? CurrencyCode.Cad,
-    amount: price.amount ?? faker.finance.amount(),
+    amount: price.amount ?? '9.99',
   };
 }
