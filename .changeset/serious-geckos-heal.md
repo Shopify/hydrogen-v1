52294d48470a11310d766096d2d28a6dc6d533a1
Fran Dios <fran.dios@shopify.com>
2022-11-15 23:34:45 +0900

    Fix RSC responses where the pathname contains double quotes (#2320)
    * Do not decode URI before parsing
    
    * Changeset
    
    * Decode every property in the state

---
+++ .changeset/serious-geckos-heal.md
@@ -0,0 +1,5 @@
+---
+'@shopify/hydrogen': patch
+---
+
+Fix RSC responses in some situations where the pathname contains double quotes.
--- packages/hydrogen/src/utilities/parse.ts
+++ packages/hydrogen/src/utilities/parse.ts
@@ -1,3 +1,5 @@
+import {RSC_PATHNAME} from '../constants.js';
+
 export function parseJSON(json: any) {
   if (String(json).includes('__proto__')) return JSON.parse(json, noproto);
   return JSON.parse(json);
@@ -8,7 +10,18 @@ function noproto(k: string, v: string) {
 
 export function parseState(url: URL) {
   try {
-    return parseJSON(url.searchParams.get('state') ?? '');
+    const {pathname, search} = url;
+    const state: Record<string, any> =
+      pathname === RSC_PATHNAME
+        ? parseJSON(url.searchParams.get('state') ?? '{}')
+        : {pathname, search};
+
+    return Object.fromEntries(
+      Object.entries(state).map(([key, value]) => [
+        decodeURIComponent(key ?? ''),
+        decodeURIComponent(value ?? ''),
+      ])
+    );
   } catch {
     // Do not throw to prevent unhandled errors
   }
