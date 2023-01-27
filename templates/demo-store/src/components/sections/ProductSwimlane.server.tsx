896815b20f4229bb9ce860e749184e17fec8c1cb
Ryan Leichty <ryan.leichty@gmail.com>
2023-01-27 12:50:42 -0800

    Import the unused ProductCards component
    - Imported the ProductCards component which is currently unused
    - Deleted the duplicate component declaration within the ProductSwimlane component

--- templates/demo-store/src/components/sections/ProductSwimlane.server.tsx
+++ templates/demo-store/src/components/sections/ProductSwimlane.server.tsx
@@ -1,7 +1,7 @@
 import {Suspense, useMemo} from 'react';
 import {gql, useShopQuery, useLocalization} from '@shopify/hydrogen';
 import {PRODUCT_CARD_FRAGMENT} from '~/lib/fragments';
-import {ProductCard, Section} from '~/components';
+import {ProductCards, Section} from '~/components';
 import type {
   Product,
   ProductConnection,
@@ -41,20 +41,6 @@ export function ProductSwimlane({
         {productCardsMarkup}
       </div>
     </Section>
-  );
-}
-
-function ProductCards({products}: {products: Product[]}) {
-  return (
-    <>
-      {products.map((product) => (
-        <ProductCard
-          product={product}
-          key={product.id}
-          className={'snap-start w-80'}
-        />
-      ))}
-    </>
   );
 }
 
