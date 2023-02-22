0dafa2bb97ee98c5a5ec3e97087b57b33a0b9475
Graham F. Scott <gfscott@gmail.com>
2023-02-22 15:54:02 -0500

    More URL config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://shopify.github.io/hydrogen-v1/',
+  url: 'https://shopify.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/hydrogen-v1/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
